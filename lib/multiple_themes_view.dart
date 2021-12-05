4b1083f7fa6ad95f1d077dfb655f849eafe2de31
FlutLab.io <>
2021-12-05 18:53:28 +0000

    Autosave before checking out to theme controller

--- lib/multiple_themes_view.dart
+++ lib/multiple_themes_view.dart
@@ -74,7 +74,7 @@ class MultipleThemesView extends StatelessWidget {
                                           height: 80,
                                           padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
                                           decoration: BoxDecoration(
-                                            color: Theme.of(context).accentColor,
+                                            color: Colors.yellow,
                                             borderRadius: BorderRadius.only(
                                               topLeft: Radius.circular(100),
                                               bottomLeft: Radius.circular(100),
