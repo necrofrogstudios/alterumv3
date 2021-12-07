582935db56cf44fdc706f56d2563a0ae08459356
FlutLab.io <>
2021-12-07 08:29:35 +0000

    Autosave

--- lib/settings/multiple_themes_view.dart
+++ lib/settings/multiple_themes_view.dart
@@ -130,9 +130,12 @@ class themes extends StatelessWidget {
           title: const Text('AlertDialog Title'),
           content: const Text('AlertDialog description'),
           actions: <Widget>[
-            Container(
-              width: 200,
-              height: 200,
+            SingleChildScrollView(
+              physics: ScrollPhysics(),
+              child: Container(
+                width: 200,
+                height: 200,
+              ),
             ),
             OutlinedButton(
               onPressed: () => Navigator.pop(context, 'Cancel'),
