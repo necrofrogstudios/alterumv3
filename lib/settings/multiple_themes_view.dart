2c2cc5d0f135412764bc68accb1af7c4ee881918
FlutLab.io <>
2021-12-07 08:59:54 +0000

    Autosave

--- lib/settings/multiple_themes_view.dart
+++ lib/settings/multiple_themes_view.dart
@@ -109,72 +109,70 @@ class MultipleThemesView extends StatelessWidget {
                 mainAxisSpacing: 5,
                 crossAxisCount: 4,
                 children: <Widget>[
-                  Column(
-                    model.themes
-                        .map(
-                          (themeData) => GestureDetector(
-                            onTap: () {
-                              getThemeManager(context).selectThemeAtIndex(themeData.index);
-                            },
-                            child: Column(
-                              children: [
-                                Stack(
-                                  children: [
-                                    Container(
-                                      decoration: BoxDecoration(
+                  model.themes
+                      .map(
+                        (themeData) => GestureDetector(
+                          onTap: () {
+                            getThemeManager(context).selectThemeAtIndex(themeData.index);
+                          },
+                          child: Column(
+                            children: [
+                              Stack(
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                        color: getThemes()[themeData.index].splashColor,
+                                        border: Border.all(
                                           color: getThemes()[themeData.index].splashColor,
-                                          border: Border.all(
-                                            color: getThemes()[themeData.index].splashColor,
-                                          ),
-                                          borderRadius: BorderRadius.all(Radius.circular(50))),
-                                      width: 50,
-                                      height: 50,
-                                      child: Row(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        crossAxisAlignment: CrossAxisAlignment.center,
-                                        children: [
-                                          Container(
-                                            width: 20,
-                                            height: 40,
-                                            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                                            decoration: BoxDecoration(
-                                              borderRadius: BorderRadius.only(
-                                                topLeft: Radius.circular(100),
-                                                bottomLeft: Radius.circular(100),
-                                              ),
-                                              color: getThemes()[themeData.index].accentColor,
+                                        ),
+                                        borderRadius: BorderRadius.all(Radius.circular(50))),
+                                    width: 50,
+                                    height: 50,
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        Container(
+                                          width: 20,
+                                          height: 40,
+                                          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                                          decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.only(
+                                              topLeft: Radius.circular(100),
+                                              bottomLeft: Radius.circular(100),
                                             ),
+                                            color: getThemes()[themeData.index].accentColor,
                                           ),
-                                          Container(
-                                            width: 20,
-                                            height: 40,
-                                            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                                            decoration: BoxDecoration(
-                                              borderRadius: BorderRadius.only(
-                                                topRight: Radius.circular(100),
-                                                bottomRight: Radius.circular(100),
-                                              ),
-                                              color: getThemes()[themeData.index].backgroundColor,
+                                        ),
+                                        Container(
+                                          width: 20,
+                                          height: 40,
+                                          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                                          decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.only(
+                                              topRight: Radius.circular(100),
+                                              bottomRight: Radius.circular(100),
                                             ),
+                                            color: getThemes()[themeData.index].backgroundColor,
                                           ),
-                                        ],
-                                      ),
+                                        ),
+                                      ],
                                     ),
-                                    Padding(
-                                      padding: EdgeInsets.fromLTRB(17, 4, 0, 0),
-                                      child: Text(
-                                        'T',
-                                        style: TextStyle(color: getThemes()[themeData.index].primaryColor, fontSize: 30),
-                                      ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(17, 4, 0, 0),
+                                    child: Text(
+                                      'T',
+                                      style: TextStyle(color: getThemes()[themeData.index].primaryColor, fontSize: 30),
                                     ),
-                                  ],
-                                ),
-                              ],
-                            ),
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
-                        )
-                        .toList(),
-                  ),
+                        ),
+                      )
+                      .toList(),
                 ],
               ),
             ],
