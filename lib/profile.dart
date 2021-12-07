5a72990ea991149921839dbf0732732c92a563e9
FlutLab.io <>
2021-12-07 06:17:45 +0000

    Autosave

--- lib/profile.dart
+++ lib/profile.dart
@@ -50,7 +50,7 @@ class profile extends StatelessWidget {
               Container(color: theme.splashColor, height: 3),
               profile_top_buttons(),
               profile_pics_icons(),
-              Container(color: theme.splashColor, height: 3),
+              Container(color: theme.splashColor, height: 5),
               profile_tabbar(),
               Container(color: theme.splashColor, height: 3),
               Container(
