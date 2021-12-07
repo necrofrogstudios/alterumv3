5c2f7e7294a84eb07e52ebfc09cb0aae4b34b556
FlutLab.io <>
2021-12-07 08:52:43 +0000

    Autosave

--- lib/settings/multiple_themes_view.dart
+++ lib/settings/multiple_themes_view.dart
@@ -103,7 +103,7 @@ class MultipleThemesView extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Wrap(
-                spacing: 5,
+                spacing: 0,
                 runSpacing: 10,
                 alignment: WrapAlignment.start,
                 direction: Axis.horizontal,
