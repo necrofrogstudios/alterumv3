c31efcc2c889d75a9c731dc8854eac974301b959
FlutLab.io <>
2021-12-07 08:39:57 +0000

    Autosave

--- lib/settings/multiple_themes_view.dart
+++ lib/settings/multiple_themes_view.dart
@@ -56,81 +56,83 @@ class MultipleThemesView extends StatelessWidget {
     Color myColor = Theme.of(context).accentColor;
     return ViewModelBuilder<MultipleThemesViewModel>.reactive(
       builder: (context, model, child) => Scaffold(
-        body: child: SingleChildScrollView(
-          physics: ScrollPhysics(),child:Column(
-          children: <Widget>[
-            Container(color: theme.splashColor, height: 4),
-            Wrap(
-              spacing: 30,
-              runSpacing: 20,
-              alignment: WrapAlignment.start,
-              direction: Axis.horizontal,
-              children: model.themes
-                  .map(
-                    (themeData) => GestureDetector(
-                      onTap: () {
-                        getThemeManager(context).selectThemeAtIndex(themeData.index);
-                      },
-                      child: Column(
-                        children: [
-                          Stack(
-                            children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                    color: getThemes()[themeData.index].splashColor,
-                                    border: Border.all(
+        body: SingleChildScrollView(
+          physics: ScrollPhysics(),
+          child: Column(
+            children: <Widget>[
+              Container(color: theme.splashColor, height: 4),
+              Wrap(
+                spacing: 30,
+                runSpacing: 20,
+                alignment: WrapAlignment.start,
+                direction: Axis.horizontal,
+                children: model.themes
+                    .map(
+                      (themeData) => GestureDetector(
+                        onTap: () {
+                          getThemeManager(context).selectThemeAtIndex(themeData.index);
+                        },
+                        child: Column(
+                          children: [
+                            Stack(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
                                       color: getThemes()[themeData.index].splashColor,
-                                    ),
-                                    borderRadius: BorderRadius.all(Radius.circular(50))),
-                                width: 50,
-                                height: 50,
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Container(
-                                      width: 20,
-                                      height: 40,
-                                      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.only(
-                                          topLeft: Radius.circular(100),
-                                          bottomLeft: Radius.circular(100),
-                                        ),
-                                        color: getThemes()[themeData.index].accentColor,
+                                      border: Border.all(
+                                        color: getThemes()[themeData.index].splashColor,
                                       ),
-                                    ),
-                                    Container(
-                                      width: 20,
-                                      height: 40,
-                                      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.only(
-                                          topRight: Radius.circular(100),
-                                          bottomRight: Radius.circular(100),
+                                      borderRadius: BorderRadius.all(Radius.circular(50))),
+                                  width: 50,
+                                  height: 50,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      Container(
+                                        width: 20,
+                                        height: 40,
+                                        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(100),
+                                            bottomLeft: Radius.circular(100),
+                                          ),
+                                          color: getThemes()[themeData.index].accentColor,
                                         ),
-                                        color: getThemes()[themeData.index].backgroundColor,
                                       ),
-                                    ),
-                                  ],
+                                      Container(
+                                        width: 20,
+                                        height: 40,
+                                        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.only(
+                                            topRight: Radius.circular(100),
+                                            bottomRight: Radius.circular(100),
+                                          ),
+                                          color: getThemes()[themeData.index].backgroundColor,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.fromLTRB(17, 4, 0, 0),
-                                child: Text(
-                                  'T',
-                                  style: TextStyle(color: getThemes()[themeData.index].primaryColor, fontSize: 30),
+                                Padding(
+                                  padding: EdgeInsets.fromLTRB(17, 4, 0, 0),
+                                  child: Text(
+                                    'T',
+                                    style: TextStyle(color: getThemes()[themeData.index].primaryColor, fontSize: 30),
+                                  ),
                                 ),
-                              ),
-                            ],
-                          ),
-                        ],
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  )
-                  .toList(),
-            ),
-          ],),
+                    )
+                    .toList(),
+              ),
+            ],
+          ),
         ),
       ),
       viewModelBuilder: () => MultipleThemesViewModel(),
