bcd79842f6b43be11f6d2900e2e0581832b6fbae
FlutLab.io <>
2021-12-07 03:56:15 +0000

    Autosave

--- lib/settings/multiple_themes_view.dart
+++ lib/settings/multiple_themes_view.dart
@@ -99,7 +99,7 @@ class MultipleThemesView extends StatelessWidget {
                                     ),
                                   ),
                                   Padding(
-                                    padding: EdgeInsets.fromLTRB(18, 30, 18, 30),
+                                    padding: EdgeInsets.fromLTRB(30, 30, 30, 30),
                                     child: Text(
                                       'text',
                                       style: TextStyle(color: getThemes()[themeData.index].primaryColor, fontSize: 20),
