2467f3cb33c52f8e387f89f2615e2652435f9485
kevinlalik <kevinlalik@gmail.com>
2021-12-08 01:42:58 -0600

    cleaning

--- lib/settings/multiple_themes_view.dart
+++ lib/settings/multiple_themes_view.dart
@@ -19,9 +19,6 @@ class _settingsState extends State<settings> {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-
-    Color myColor = Theme.of(context).accentColor;
-
     return Scaffold(
       backgroundColor: theme.backgroundColor,
       appBar: AppBar(
@@ -154,8 +151,6 @@ class _MultipleThemesViewState extends State<MultipleThemesView> {
 
   @override
   Widget build(BuildContext context) {
-    var theme = Theme.of(context);
-    Color myColor = Theme.of(context).accentColor;
     return ViewModelBuilder<MultipleThemesViewModel>.reactive(
       builder: (context, model, child) => Scaffold(
         body: SingleChildScrollView(
@@ -174,7 +169,8 @@ class _MultipleThemesViewState extends State<MultipleThemesView> {
                       .map(
                         (themeData) => GestureDetector(
                           onTap: () {
-                            getThemeManager(context).selectThemeAtIndex(themeData.index);
+                            getThemeManager(context)
+                                .selectThemeAtIndex(themeData.index);
                           },
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.center,
@@ -183,39 +179,48 @@ class _MultipleThemesViewState extends State<MultipleThemesView> {
                                 children: [
                                   Container(
                                     decoration: BoxDecoration(
-                                        color: getThemes[themeData.index].splashColor,
+                                        color: getThemes[themeData.index]
+                                            .splashColor,
                                         border: Border.all(
-                                          color: getThemes[themeData.index].splashColor,
+                                          color: getThemes[themeData.index]
+                                              .splashColor,
                                         ),
-                                        borderRadius: BorderRadius.all(Radius.circular(50))),
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(50))),
                                     width: 50,
                                     height: 50,
                                     child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
                                       children: [
                                         Container(
                                           width: 20,
                                           height: 40,
-                                          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 10, vertical: 10),
                                           decoration: BoxDecoration(
                                             borderRadius: BorderRadius.only(
                                               topLeft: Radius.circular(100),
                                               bottomLeft: Radius.circular(100),
                                             ),
-                                            color: getThemes[themeData.index].accentColor,
+                                            color: getThemes[themeData.index]
+                                                .accentColor,
                                           ),
                                         ),
                                         Container(
                                           width: 20,
                                           height: 40,
-                                          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                                          padding: EdgeInsets.symmetric(
+                                              horizontal: 10, vertical: 10),
                                           decoration: BoxDecoration(
                                             borderRadius: BorderRadius.only(
                                               topRight: Radius.circular(100),
                                               bottomRight: Radius.circular(100),
                                             ),
-                                            color: getThemes[themeData.index].backgroundColor,
+                                            color: getThemes[themeData.index]
+                                                .backgroundColor,
                                           ),
                                         ),
                                       ],
@@ -225,7 +230,10 @@ class _MultipleThemesViewState extends State<MultipleThemesView> {
                                     padding: EdgeInsets.fromLTRB(17, 4, 0, 0),
                                     child: Text(
                                       'T',
-                                      style: TextStyle(color: getThemes[themeData.index].primaryColor, fontSize: 30),
+                                      style: TextStyle(
+                                          color: getThemes[themeData.index]
+                                              .primaryColor,
+                                          fontSize: 30),
                                     ),
                                   ),
                                 ],
