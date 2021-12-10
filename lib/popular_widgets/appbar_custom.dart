99bbbdcbc99c14c9248cbdb9bee407f4cf1d8fbc
FlutLab.io <>
2021-12-10 10:36:04 +0000

    Autosave

--- lib/popular_widgets/appbar_custom.dart
+++ lib/popular_widgets/appbar_custom.dart
@@ -14,37 +14,39 @@ import 'package:marquee/marquee.dart';
 
 class marquee extends StatelessWidget {
   Widget build(BuildContext context) {
-     var theme = Theme.of(context);
+    var theme = Theme.of(context);
     Color myColor = Theme.of(context).accentColor;
     var theme = Theme.of(context);
-Return AppBar(
-        iconTheme: IconThemeData(color: theme.primaryColor),
-        centerTitle: true,
-        title: FlatButton(
-          splashColor: Colors.transparent,
-          highlightColor: Colors.transparent,
-          hoverColor: Colors.transparent,
-          child: Text(
-            'Alterum',
-            style: TextStyle(color: theme.primaryColor, fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          onPressed: () {},
+    return AppBar(
+      iconTheme: IconThemeData(color: theme.primaryColor),
+      centerTitle: true,
+      title: FlatButton(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        hoverColor: Colors.transparent,
+        child: Text(
+          'Alterum',
+          style: TextStyle(color: theme.primaryColor, fontSize: 20, fontWeight: FontWeight.bold),
         ),
-        backgroundColor: myColor,
-        actions: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(0, 0, 8, 0),
-            child: SimpleAccountMenu(
-              icons: [
-                Icon(Icons.person),
-                Icon(Icons.email),
-                Icon(Icons.group),
-              ],
-              iconColor: theme.primaryColor,
-              onChange: (index) {
-                print(index);
-              },
-            ),
+        onPressed: () {},
+      ),
+      backgroundColor: myColor,
+      actions: <Widget>[
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 0, 8, 0),
+          child: SimpleAccountMenu(
+            icons: [
+              Icon(Icons.person),
+              Icon(Icons.email),
+              Icon(Icons.group),
+            ],
+            iconColor: theme.primaryColor,
+            onChange: (index) {
+              print(index);
+            },
           ),
-        ],
-      );}}
+        ),
+      ],
+    );
+  }
+}
