eec6e7169ff0c721770f0cb277b7e5f9628baa85
FlutLab.io <>
2021-12-10 10:58:57 +0000

    Autosave

--- lib/main_page/marquee.dart
+++ lib/main_page/marquee.dart
@@ -15,32 +15,35 @@ class marquee extends StatelessWidget {
 }
 
 Widget _buildComplexMarquee() {
-  return Container(
-    color: theme.accentColor,
-    child: Marquee(
-      text: 'Oh kevin loves the kevin song song '
-          'lets gather around the kevin, and sing the kevin song song '
-          'the k-e-v-i-n s-o-n-g song song song song song',
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 15,
-        color: theme.primaryColor,
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    return Container(
+      color: theme.accentColor,
+      child: Marquee(
+        text: 'Oh kevin loves the kevin song song '
+            'lets gather around the kevin, and sing the kevin song song '
+            'the k-e-v-i-n s-o-n-g song song song song song',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 15,
+          color: theme.primaryColor,
+        ),
+        scrollAxis: Axis.horizontal,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        blankSpace: 170.0,
+        velocity: 60.0,
+        pauseAfterRound: Duration(seconds: 5),
+        showFadingOnlyWhenScrolling: true,
+        fadingEdgeStartFraction: 0.1,
+        fadingEdgeEndFraction: 0.1,
+        startPadding: 10.0,
+        accelerationDuration: Duration(seconds: 1),
+        accelerationCurve: Curves.linear,
+        decelerationDuration: Duration(milliseconds: 500),
+        decelerationCurve: Curves.easeOut,
       ),
-      scrollAxis: Axis.horizontal,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      blankSpace: 170.0,
-      velocity: 60.0,
-      pauseAfterRound: Duration(seconds: 5),
-      showFadingOnlyWhenScrolling: true,
-      fadingEdgeStartFraction: 0.1,
-      fadingEdgeEndFraction: 0.1,
-      startPadding: 10.0,
-      accelerationDuration: Duration(seconds: 1),
-      accelerationCurve: Curves.linear,
-      decelerationDuration: Duration(milliseconds: 500),
-      decelerationCurve: Curves.easeOut,
-    ),
-  );
+    );
+  }
 }
 
 // Styling the Marquee
