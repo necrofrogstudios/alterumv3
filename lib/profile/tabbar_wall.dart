e3bf4d7a268d67579e2c7a296508773c3c0e505a
FlutLab.io <>
2021-12-10 04:42:14 +0000

    Autosave

--- lib/profile/tabbar_wall.dart
+++ lib/profile/tabbar_wall.dart
@@ -116,74 +116,77 @@ class comments extends StatelessWidget {
 
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    return Container(
-      child: Card(
-        shape: RoundedRectangleBorder(
-          side: BorderSide(
-            color: theme.splashColor,
-            width: 4.0,
-          ),
-          borderRadius: BorderRadius.circular(
-            30.0,
-          ),
-        ),
-        color: theme.accentColor,
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 10, 0, 0),
-                      child: CircleAvatar(
-                        radius: 20,
-                        backgroundImage: NetworkImage(comment.profilePhoto),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
-                          child: Text(
-                            'Guarded',
-                            style: TextStyle(color: theme.primaryColor, fontSize: 15),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                          child: Text(
-                            DateFormat('MM/dd/yyyy - hh:mm a').format(comment.date),
-                            style: TextStyle(fontSize: 15, color: theme.primaryColor.withOpacity(0.45)),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                Container(
-                  height: 75,
-                  width: 75,
-                  decoration: BoxDecoration(color: theme.accentColor, borderRadius: BorderRadius.all(Radius.circular(30))),
-                  child: IconButton(
-                    iconSize: 15,
-                    icon: Icon(Icons.close, color: theme.primaryColor),
-                    onPressed: () {},
-                  ),
-                ),
-              ],
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+      child: Container(
+        child: Card(
+          shape: RoundedRectangleBorder(
+            side: BorderSide(
+              color: theme.splashColor,
+              width: 4.0,
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 0, 10, 20),
-              child: SelectableText(
-                comment.bodyText,
-                style: TextStyle(color: theme.primaryColor, fontSize: 15),
+            borderRadius: BorderRadius.circular(
+              30.0,
+            ),
+          ),
+          color: theme.accentColor,
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 10, 0, 0),
+                        child: CircleAvatar(
+                          radius: 20,
+                          backgroundImage: NetworkImage(comment.profilePhoto),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+                            child: Text(
+                              'Guarded',
+                              style: TextStyle(color: theme.primaryColor, fontSize: 15),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                            child: Text(
+                              DateFormat('MM/dd/yyyy - hh:mm a').format(comment.date),
+                              style: TextStyle(fontSize: 15, color: theme.primaryColor.withOpacity(0.45)),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Container(
+                    height: 75,
+                    width: 75,
+                    decoration: BoxDecoration(color: theme.accentColor, borderRadius: BorderRadius.all(Radius.circular(30))),
+                    child: IconButton(
+                      iconSize: 15,
+                      icon: Icon(Icons.close, color: theme.primaryColor),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
               ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 10, 20),
+                child: SelectableText(
+                  comment.bodyText,
+                  style: TextStyle(color: theme.primaryColor, fontSize: 15),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
