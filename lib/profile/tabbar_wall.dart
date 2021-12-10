7290f05e87f5c59a11b1b78be9799660757ae7b2
FlutLab.io <>
2021-12-10 02:54:52 +0000

    Autosave

--- lib/profile/tabbar_wall.dart
+++ lib/profile/tabbar_wall.dart
@@ -120,12 +120,12 @@ class comments extends StatelessWidget {
     return Container(
       child: Card(
         shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: Colors.black,
+            width: 2.0,
+          ),
           borderRadius: BorderRadius.circular(
             30.0,
-            side: BorderSide(
-              color: Colors.black,
-              width: 2.0,
-            ),
           ),
         ),
         color: Colors.grey[850],
