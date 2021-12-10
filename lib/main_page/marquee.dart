57f37b54cb5a5bd5dfae334a7c68a4c9e534530d
FlutLab.io <>
2021-12-10 11:04:36 +0000

    Autosave

--- lib/main_page/marquee.dart
+++ lib/main_page/marquee.dart
@@ -15,31 +15,28 @@ class marquee extends StatelessWidget {
 }
 
 Widget _buildComplexMarquee(ThemeData theme) {
-  return Container(
-    color: theme.accentColor,
-    child: Marquee(
-      text: 'Oh kevin loves the kevin song song '
-          'lets gather around the kevin, and sing the kevin song song '
-          'the k-e-v-i-n s-o-n-g song song song song song',
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 15,
-        color: theme.primaryColor,
-      ),
-      scrollAxis: Axis.horizontal,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      blankSpace: 170.0,
-      velocity: 60.0,
-      pauseAfterRound: Duration(seconds: 5),
-      showFadingOnlyWhenScrolling: true,
-      fadingEdgeStartFraction: 0.1,
-      fadingEdgeEndFraction: 0.1,
-      startPadding: 10.0,
-      accelerationDuration: Duration(seconds: 1),
-      accelerationCurve: Curves.linear,
-      decelerationDuration: Duration(milliseconds: 500),
-      decelerationCurve: Curves.easeOut,
+  return Marquee(
+    text: 'Oh kevin loves the kevin song song '
+        'lets gather around the kevin, and sing the kevin song song '
+        'the k-e-v-i-n s-o-n-g song song song song song',
+    style: TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 15,
+      color: theme.primaryColor,
     ),
+    scrollAxis: Axis.horizontal,
+    crossAxisAlignment: CrossAxisAlignment.start,
+    blankSpace: 170.0,
+    velocity: 60.0,
+    pauseAfterRound: Duration(seconds: 5),
+    showFadingOnlyWhenScrolling: true,
+    fadingEdgeStartFraction: 0.1,
+    fadingEdgeEndFraction: 0.1,
+    startPadding: 10.0,
+    accelerationDuration: Duration(seconds: 1),
+    accelerationCurve: Curves.linear,
+    decelerationDuration: Duration(milliseconds: 500),
+    decelerationCurve: Curves.easeOut,
   );
 }
 
