ac332e913137910827e2a8489d34b8f57d24b55e
FlutLab.io <>
2021-12-10 10:35:11 +0000

    Autosave

--- lib/popular_widgets/appbar_custom.dart
+++ lib/popular_widgets/appbar_custom.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:marquee/marquee.dart';
 
+import 'package:testing/popular_widgets/drawer.dart';
+import 'package:testing/popular_widgets/appbar_dropdown_button.dart';
+import 'package:testing/popular_widgets/appbar_custom.dart';
+import 'package:testing/popular_widgets/appbar_top_buttons.dart';
+import 'package:testing/profile/profile_pics_icons.dart';
+import 'package:testing/profile/profile_tabbar.dart';
+import 'package:testing/main_page/marquee.dart';
+
+import 'package:flutter/material.dart';
+import 'package:marquee/marquee.dart';
+
 class marquee extends StatelessWidget {
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
