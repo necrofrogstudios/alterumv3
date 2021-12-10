0c46f74d0f819a259f7f9a21180fee7fd5d387ab
FlutLab.io <>
2021-12-10 02:21:45 +0000

    Autosave

--- lib/profile/profile_tabbar.dart
+++ lib/profile/profile_tabbar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:testing/profile/tabbar_wall.dart';
 
 class profile_tabbar extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -43,107 +43,6 @@ class profile_tabbar extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-class wall_tab extends StatefulWidget {
-  @override
-  State<wall_tab> createState() => _wall_tabState();
-}
-
-class _wall_tabState extends State<wall_tab> {
-  Widget build(BuildContext context) {
-    final textController = TextEditingController();
-    String a = textController.text;
-    var theme = Theme.of(context);
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      child: SingleChildScrollView(
-        physics: ScrollPhysics(),
-        child: Column(
-          children: [
-            Text('test'),
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
-              child: TextFormField(
-                controller: textController,
-                style: TextStyle(color: theme.primaryColor),
-                //overflow bug if too much text is entered in this field
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(25.0)),
-                    borderSide: BorderSide(color: theme.accentColor, width: 3.0),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(25.0)),
-                    borderSide: BorderSide(color: theme.accentColor, width: 3.0),
-                  ),
-                  hintText: 'Type a Message Here',
-                  hintStyle: TextStyle(
-                    color: Colors.grey,
-                  ),
-                  contentPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20),
-                ),
-              ),
-            ),
-            Text('test'),
-            Container(
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(0, 15, 30, 0),
-                child: Align(
-                  alignment: Alignment.centerRight,
-                  child: OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                      side: BorderSide(width: 3.0, color: theme.accentColor),
-                      elevation: 6,
-                      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 17),
-                      backgroundColor: theme.backgroundColor,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                    ),
-                    child: Text(
-                      'Post',
-                      style: TextStyle(fontSize: 15, color: theme.primaryColor),
-                    ),
-                    onPressed: () {
-                      addComment(textController.text);
-                    },
-                  ),
-                ),
-              ),
-            ),
-            ListView.builder(
-              itemCount: commentList.length,
-              itemBuilder: (context, index) {
-                return comments(commentList[commentListIndexChecker(commentList, index)]);
-              },
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-            ),
-            //comments(commentList[0])
-          ],
-        ),
-      ),
-    );
-  }
-
-  int commentListIndexChecker(List<WallComment> list, int i) {
-    if (list.length == 0) {
-      return 0;
-    } else {
-      return ((list.length - i) - 1);
-    }
-  }
-
-  void addComment(String postText) {
-    final newComment = WallComment(id: '1', profilePhoto: 'https://bestprofilepictures.com/wp-content/uploads/2021/04/Cool-Picture.jpg', bodyText: postText, date: DateTime.now());
-    setState(() {
-      commentList.add(newComment);
-    });
   }
 }
 
---
+++ lib/profile/tabbar_wall.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class wall_tab extends StatefulWidget {
+  @override
+  State<wall_tab> createState() => _wall_tabState();
+}
+
+class _wall_tabState extends State<wall_tab> {
+  Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    String a = textController.text;
+    var theme = Theme.of(context);
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: SingleChildScrollView(
+        physics: ScrollPhysics(),
+        child: Column(
+          children: [
+            Text('test'),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+              child: TextFormField(
+                controller: textController,
+                style: TextStyle(color: theme.primaryColor),
+                //overflow bug if too much text is entered in this field
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                    borderSide: BorderSide(color: theme.accentColor, width: 3.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                    borderSide: BorderSide(color: theme.accentColor, width: 3.0),
+                  ),
+                  hintText: 'Type a Message Here',
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                  contentPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20),
+                ),
+              ),
+            ),
+            Text('test'),
+            Container(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(0, 15, 30, 0),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(width: 3.0, color: theme.accentColor),
+                      elevation: 6,
+                      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 17),
+                      backgroundColor: theme.backgroundColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: Text(
+                      'Post',
+                      style: TextStyle(fontSize: 15, color: theme.primaryColor),
+                    ),
+                    onPressed: () {
+                      addComment(textController.text);
+                    },
+                  ),
+                ),
+              ),
+            ),
+            ListView.builder(
+              itemCount: commentList.length,
+              itemBuilder: (context, index) {
+                return comments(commentList[commentListIndexChecker(commentList, index)]);
+              },
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+            ),
+            //comments(commentList[0])
+          ],
+        ),
+      ),
+    );
+  }
+
+  int commentListIndexChecker(List<WallComment> list, int i) {
+    if (list.length == 0) {
+      return 0;
+    } else {
+      return ((list.length - i) - 1);
+    }
+  }
+
+  void addComment(String postText) {
+    final newComment = WallComment(id: '1', profilePhoto: 'https://bestprofilepictures.com/wp-content/uploads/2021/04/Cool-Picture.jpg', bodyText: postText, date: DateTime.now());
+    setState(() {
+      commentList.add(newComment);
+    });
+  }
+}
+
+class WallComment {
+  String id;
+  String profilePhoto;
+  String bodyText;
+  DateTime date;
+
+  WallComment({this.id, this.profilePhoto, this.bodyText, this.date});
+}
+
+class comments extends StatelessWidget {
+  WallComment comment;
+  comments(this.comment);
+
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    return Container(
+      child: Card(
+        color: Colors.grey[850],
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CircleAvatar(
+                    radius: 20,
+                    backgroundImage: NetworkImage(comment.profilePhoto),
+                  ),
+                ),
+                Text(
+                  DateFormat('MM/dd/yyyy - hh:mm a').format(comment.date),
+                  style: TextStyle(color: theme.primaryColor),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                comment.bodyText,
+                style: TextStyle(color: theme.primaryColor),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+final List<WallComment> commentList = [];
