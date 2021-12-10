eabd8a2f3620f4fa33e81153a8b37e3c12029f08
FlutLab.io <>
2021-12-10 02:25:32 +0000

    Autosave

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,6 +1,6 @@
 import 'package:splashscreen/splashscreen.dart';
 import 'package:flutter/material.dart';
-import 'package:testing/profile/profile.dart';
+import 'package:testing/profile.dart';
 
 class Splash extends StatefulWidget {
   @override
