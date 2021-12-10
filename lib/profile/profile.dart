07f2f7d6dd571d8bdb3655647ee463364e97d052
FlutLab.io <>
2021-12-10 10:49:01 +0000

    Autosave

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -5,6 +5,7 @@ import 'package:testing/popular_widgets/appbar_dropdown_button.dart';
 import 'package:testing/profile/profile_pics_icons.dart';
 import 'package:testing/profile/profile_tabbar.dart';
 import 'package:testing/popular_widgets/appbar_top_buttons.dart';
+import 'package:testing/popular_widgets/appbar_custom.dart';
 
 class profile extends StatelessWidget {
   final currentScreen = profile;
@@ -14,36 +15,9 @@ class profile extends StatelessWidget {
     Color myColor = Theme.of(context).accentColor;
 
     return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: theme.primaryColor),
-        centerTitle: true,
-        title: FlatButton(
-          splashColor: Colors.transparent,
-          highlightColor: Colors.transparent,
-          hoverColor: Colors.transparent,
-          child: Text(
-            'Alterum',
-            style: TextStyle(color: theme.primaryColor, fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          onPressed: () {},
-        ),
-        backgroundColor: myColor,
-        actions: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(0, 0, 8, 0),
-            child: SimpleAccountMenu(
-              icons: [
-                Icon(Icons.person),
-                Icon(Icons.email),
-                Icon(Icons.group),
-              ],
-              iconColor: theme.primaryColor,
-              onChange: (index) {
-                print(index);
-              },
-            ),
-          ),
-        ],
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(55),
+        child: appbar_custom(),
       ),
       drawer: drawer(currentScreen),
       body: Container(
