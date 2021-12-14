ef557b0ca7118f9f18d966442f80dffae705e93e
FlutLab.io <>
2021-12-14 05:57:44 +0000

    Autosave

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:testing/popular_widgets/drawer.dart';
 import 'package:testing/popular_widgets/appbar_dropdown_button.dart';
+import 'package:testing/popular_widgets/footer.dart';
 import 'package:testing/profile/profile_pics_icons.dart';
 import 'package:testing/profile/profile_tabbar.dart';
 import 'package:testing/popular_widgets/appbar_top_buttons.dart';
@@ -33,28 +34,7 @@ class profile extends StatelessWidget {
               Container(color: theme.splashColor, height: 4),
               profile_tabbar(),
               Container(color: theme.splashColor, height: 4),
-              Container(
-                color: theme.accentColor,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      '\u00a9 2022 Alterum',
-                      style: TextStyle(
-                        color: theme.primaryColor.withOpacity(0.45),
-                        fontSize: 15,
-                      ),
-                    ),
-                    FlatButton(
-                      child: Text(
-                        'Contact Support',
-                        style: TextStyle(color: theme.primaryColor),
-                      ),
-                      onPressed: () {},
-                    )
-                  ],
-                ),
-              ),
+              footer(),
             ],
           ),
         ),
