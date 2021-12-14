ec5c9dc88a9980b9c5baaa895f3fcf66b0d0396f
FlutLab.io <>
2021-12-14 09:07:19 +0000

    Autosave

--- lib/main_page/roleplay_list_layout.dart
+++ lib/main_page/roleplay_list_layout.dart
@@ -30,7 +30,7 @@ class roleplay_list_layout extends StatelessWidget {
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
                   child: Text(
                     'Creators: Guarded, basketcase101, MrCarrot, DeadPan',
                     textAlign: TextAlign.center,
@@ -38,7 +38,7 @@ class roleplay_list_layout extends StatelessWidget {
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.fromLTRB(10, 5, 10, 10),
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
                   child: Text(
                     'Tags: action, adventure, first pov, active',
                     textAlign: TextAlign.center,
