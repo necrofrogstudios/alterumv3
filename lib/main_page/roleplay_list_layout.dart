22f83eb5afabb9022c001b701cf1f429406f37d7
FlutLab.io <>
2021-12-14 08:49:08 +0000

    Autosave

--- lib/main_page/roleplay_list_layout.dart
+++ lib/main_page/roleplay_list_layout.dart
@@ -22,7 +22,7 @@ class roleplay_list_layout extends StatelessWidget {
             child: Column(
               children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 5),
                   child: Text(
                     '【僕の戦争】─ My War ─│FREEVERSE ANIME RP | HAPPY ONE YEAR !!!!',
                     style: TextStyle(color: theme.primaryColor, fontSize: 15),
