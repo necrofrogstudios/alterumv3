bde3de5e4638c0ace5f444c46787a2a41fa8718d
FlutLab.io <>
2021-12-14 09:31:27 +0000

    Autosave

--- lib/main_page/roleplay_list_layout.dart
+++ lib/main_page/roleplay_list_layout.dart
@@ -23,13 +23,7 @@ class roleplay_list_layout extends StatelessWidget {
               children: <Widget>[
                 Stack(
                   children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(fit: BoxFit.cover, image: NetworkImage('https://th.bing.com/th/id/R.2d13e2fa22f64af810e32eac96cc6f6b?rik=eOg1FQ9TgI3hcg&pid=ImgRaw&r=0')),
-                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                        color: Colors.redAccent,
-                      ),
-                    ),
+                    Image.network('https://th.bing.com/th/id/R.2d13e2fa22f64af810e32eac96cc6f6b?rik=eOg1FQ9TgI3hcg&pid=ImgRaw&r=0'),
                     Padding(
                       padding: EdgeInsets.fromLTRB(10, 10, 10, 5),
                       child: Text(
