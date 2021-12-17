dda3796fad01a78855113d0cf769be80823c4308
FlutLab.io <>
2021-12-17 05:53:21 +0000

    Autosave

--- lib/main_page/main_page.dart
+++ lib/main_page/main_page.dart
@@ -28,7 +28,7 @@ class main_page extends StatelessWidget {
         width: double.infinity,
         height: double.infinity,
         child: SingleChildScrollView(
-          physics: ScrollPhysics(),
+          physics: BouncingScrollPhysics(),
           child: Column(
             children: <Widget>[
               Container(color: theme.splashColor, height: 4),
