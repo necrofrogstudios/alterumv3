88642424522f4756b40fe6a5cc10076d9e961b5b
FlutLab.io <>
2021-12-17 08:26:36 +0000

    Autosave

--- lib/main_page/roleplay_list_layout.dart
+++ lib/main_page/roleplay_list_layout.dart
@@ -47,7 +47,7 @@ class roleplay_list_layout extends StatelessWidget {
                       ),
                     ),
                     Padding(
-                      padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                      padding: EdgeInsets.fromLTRB(10, 0, 10, 5),
                       child: Text(
                         ' The Haunted Hotel of Jefferson',
                         textAlign: TextAlign.center,
