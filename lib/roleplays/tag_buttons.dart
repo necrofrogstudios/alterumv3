26e6f0c4536d517d0bcba75a8d79028a1b5350fe
FlutLab.io <>
2021-12-17 06:36:09 +0000

    Autosave

--- lib/roleplays/tag_buttons.dart
+++ lib/roleplays/tag_buttons.dart
@@ -19,26 +19,25 @@ class tag_buttons extends StatelessWidget {
         scrollDirection: Axis.horizontal,
         itemCount: items.length,
         itemBuilder: (BuildContext context, int index) {
-          return Expanded(
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
-              child: OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  side: BorderSide(width: 3.0, color: theme.backgroundColor),
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                  backgroundColor: theme.accentColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
+          return Expandable(
+              child: Padding(
+            padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+            child: OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                side: BorderSide(width: 3.0, color: theme.backgroundColor),
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                backgroundColor: theme.accentColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
                 ),
-                child: Text(
-                  items[index],
-                  style: TextStyle(fontSize: 12, color: theme.primaryColor),
-                ),
-                onPressed: () {},
               ),
+              child: Text(
+                items[index],
+                style: TextStyle(fontSize: 12, color: theme.primaryColor),
+              ),
+              onPressed: () {},
             ),
-          );
+          ));
         },
       ),
     );
