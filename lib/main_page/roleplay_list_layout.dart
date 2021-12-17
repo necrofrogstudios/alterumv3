812675421ab48b89e0e3a0624d67fa7cc4f3f8e7
FlutLab.io <>
2021-12-17 03:33:14 +0000

    Autosave

--- lib/main_page/roleplay_list_layout.dart
+++ lib/main_page/roleplay_list_layout.dart
@@ -10,73 +10,78 @@ class roleplay_list_layout extends StatelessWidget {
         shrinkWrap: true,
         padding: const EdgeInsets.all(8),
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
-            child: Container(
-              decoration: BoxDecoration(
-                color: theme.accentColor,
-                border: Border.all(color: theme.splashColor, width: 3.0),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(30),
+          GestureDetector(
+            onTap: () {
+              print("Container clicked");
+            },
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: theme.accentColor,
+                  border: Border.all(color: theme.splashColor, width: 3.0),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(30),
+                  ),
+                ), //name, creator, tags, intro
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                        ),
+                        image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: NetworkImage("https://timelinecovers.pro/facebook-cover/download/outer-space-planets-facebook-cover.jpg"),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(10, 10, 10, 5),
+                      child: Text(
+                        '【僕の戦争】─ My War ─│FREEVERSE ANIME RP | HAPPY ONE YEAR !!!!',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: theme.primaryColor, fontSize: 15),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                      child: Text(
+                        'Creators: Guarded, basketcase101, MrCarrot, DeadPan',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: theme.primaryColor, fontSize: 15),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                      child: Text(
+                        'Tags: action, adventure, first pov, active',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: theme.primaryColor, fontSize: 12),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                      child: Text(
+                        'With 791808 posts, 67 votes, 164 favorites, 26840 views, 411 comments',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: theme.primaryColor, fontSize: 15),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                      child: Text(
+                        'Active 2 seconds ago',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: theme.primaryColor, fontSize: 15),
+                      ),
+                    ),
+                  ],
                 ),
-              ), //name, creator, tags, intro
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30),
-                      ),
-                      image: DecorationImage(
-                        fit: BoxFit.fill,
-                        image: NetworkImage("https://timelinecovers.pro/facebook-cover/download/outer-space-planets-facebook-cover.jpg"),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(10, 10, 10, 5),
-                    child: Text(
-                      '【僕の戦争】─ My War ─│FREEVERSE ANIME RP | HAPPY ONE YEAR !!!!',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: theme.primaryColor, fontSize: 15),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                    child: Text(
-                      'Creators: Guarded, basketcase101, MrCarrot, DeadPan',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: theme.primaryColor, fontSize: 15),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
-                    child: Text(
-                      'Tags: action, adventure, first pov, active',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: theme.primaryColor, fontSize: 12),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
-                    child: Text(
-                      'With 791808 posts, 67 votes, 164 favorites, 26840 views, 411 comments',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: theme.primaryColor, fontSize: 15),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
-                    child: Text(
-                      'Active 2 seconds ago',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: theme.primaryColor, fontSize: 15),
-                    ),
-                  ),
-                ],
               ),
             ),
           ),
