22c9160e0d930e818a8a366415ec57cab63693b3
FlutLab.io <>
2021-12-17 03:38:16 +0000

    Autosave

--- lib/main_page/roleplay_list_layout.dart
+++ lib/main_page/roleplay_list_layout.dart
@@ -10,7 +10,7 @@ class roleplay_list_layout extends StatelessWidget {
         shrinkWrap: true,
         padding: const EdgeInsets.all(8),
         children: <Widget>[
-          InkWell(
+          GestureDetector(
             onTap: () {
               print("Container clicked");
             },
