9ddce1a6f1a8da72cbe2e307c123eac645fb9f20
FlutLab.io <>
2021-12-17 06:15:35 +0000

    Autosave

--- lib/main_page/roleplay_list_layout.dart
+++ lib/main_page/roleplay_list_layout.dart
@@ -68,8 +68,8 @@ class roleplay_list_layout extends StatelessWidget {
                         textAlign: TextAlign.center,
                         style: TextStyle(color: theme.primaryColor, fontSize: 15),
                       ),
-                      Container(color: theme.splashColor, height: 4),
                     ),
+                    Container(color: theme.splashColor, height: 4),
                     Padding(
                       padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
                       child: Text(
