70bf93b598f4afd5853ae32048d09bbd847517e9
FlutLab.io <>
2021-12-17 08:18:49 +0000

    Autosave

--- lib/main_page/roleplay_list_layout.dart
+++ lib/main_page/roleplay_list_layout.dart
@@ -56,14 +56,6 @@ class roleplay_list_layout extends StatelessWidget {
                     ),
                     Container(color: theme.splashColor, height: 4, width: 100),
                     Padding(
-                      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                      child: Text(
-                        'Tags: action, adventure, first pov, active',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: theme.primaryColor, fontSize: 12),
-                      ),
-                    ),
-                    Padding(
                       padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
                       child: Text(
                         'With  164 favorites, 37 active roleplayers',
