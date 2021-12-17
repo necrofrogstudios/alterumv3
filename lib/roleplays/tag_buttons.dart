2394f930b28bbff4cfc1f95d27d3433b68849d35
FlutLab.io <>
2021-12-17 07:18:56 +0000

    Autosave

--- lib/roleplays/tag_buttons.dart
+++ lib/roleplays/tag_buttons.dart
@@ -11,38 +11,29 @@ class tag_buttons extends StatelessWidget {
 
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    return Container(
-      color: theme.accentColor,
-      height: 90,
-      child: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Column(
-            children: [
-              Container(
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
-                  child: OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                      side: BorderSide(width: 3.0, color: theme.backgroundColor),
-                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                      backgroundColor: theme.accentColor,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                    ),
-                    child: Text(
-                      items[index],
-                      style: TextStyle(fontSize: 12, color: theme.primaryColor),
-                    ),
-                    onPressed: () {},
-                  ),
+    return Row(
+      children: [
+        Container(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+            child: OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                side: BorderSide(width: 3.0, color: theme.backgroundColor),
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                backgroundColor: theme.accentColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
                 ),
               ),
-            ],
-          );
-        },
-      ),
+              child: Text(
+                items[index],
+                style: TextStyle(fontSize: 12, color: theme.primaryColor),
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
