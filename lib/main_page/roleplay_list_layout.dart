2dba34aa06528278152262fe3b791f2f0ecf864f
FlutLab.io <>
2021-12-17 08:26:51 +0000

    Autosave

--- lib/main_page/roleplay_list_layout.dart
+++ lib/main_page/roleplay_list_layout.dart
@@ -75,7 +75,7 @@ class roleplay_list_layout extends StatelessWidget {
                     ),
                     Container(color: theme.splashColor, height: 4, width: 100),
                     Padding(
-                      padding: EdgeInsets.only(top: 5, bottom: 10),
+                      padding: EdgeInsets.only(top: 10, bottom: 10),
                       child: tag_buttons(),
                     ),
                   ],
