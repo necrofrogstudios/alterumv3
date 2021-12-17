e5891d467d9ed996672359fa2f714d5bd3d1e448
FlutLab.io <>
2021-12-17 03:54:15 +0000

    Autosave

--- lib/roleplays/roleplay_main.dart
+++ lib/roleplays/roleplay_main.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:marquee/marquee.dart';
 
-import 'package:testing/popular_widgets/drawer.dart';
+
+
 import 'package:testing/popular_widgets/appbar_dropdown_button.dart';
 import 'package:testing/popular_widgets/appbar_custom.dart';
 import 'package:testing/popular_widgets/appbar_top_buttons.dart';
 import 'package:testing/popular_widgets/footer.dart';
 import 'package:testing/profile/profile_pics_icons.dart';
 import 'package:testing/profile/profile_tabbar.dart';
-import 'package:testing/main_page/marquee.dart';
-import 'package:testing/main_page/roleplay_list_layout.dart';
+
 
 class roleplay_main extends StatelessWidget {
   final currentScreen = roleplay_main;
