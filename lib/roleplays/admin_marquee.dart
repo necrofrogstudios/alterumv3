fbe42c72a29ae9e7d36cb678f55cb785ba4739a5
FlutLab.io <>
2021-12-17 09:30:44 +0000

    Autosave

--- lib/roleplays/admin_marquee.dart
+++ lib/roleplays/admin_marquee.dart
@@ -27,7 +27,7 @@ Widget _buildComplexMarquee(ThemeData theme) {
     scrollAxis: Axis.horizontal,
     crossAxisAlignment: CrossAxisAlignment.start,
     blankSpace: 400.0,
-    velocity: 30.0,
+    velocity: 40.0,
     pauseAfterRound: Duration(seconds: 5),
     showFadingOnlyWhenScrolling: true,
     fadingEdgeStartFraction: 0.1,
