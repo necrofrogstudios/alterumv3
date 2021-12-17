80d7db7e25679485df0d3410475958194b6ccb15
FlutLab.io <>
2021-12-17 10:23:17 +0000

    Autosave

--- lib/roleplays/roleplay_request.dart
+++ lib/roleplays/roleplay_request.dart
@@ -5,28 +5,26 @@ class roleplay_request extends StatelessWidget {
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
     return Padding(
-                      padding: EdgeInsets.only(top: 10, bottom: 0),
-                      child: FlatButton(
-                        color: theme.accentColor,
-                        splashColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        height: 60.0,
-                        onPressed: () {
-                          
-                        },
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'Request Form',
-                              style: TextStyle(
-                                fontSize: 30,
-                                color: theme.primaryColor,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),;
+      padding: EdgeInsets.only(top: 10, bottom: 0),
+      child: FlatButton(
+        color: theme.accentColor,
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        height: 60.0,
+        onPressed: () {},
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Request Form',
+              style: TextStyle(
+                fontSize: 30,
+                color: theme.primaryColor,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
