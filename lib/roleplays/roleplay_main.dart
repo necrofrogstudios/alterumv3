7d60ca444a4565fd0ac5d9a286c613c8dd59d5c9
FlutLab.io <>
2021-12-17 05:20:21 +0000

    Autosave

--- lib/roleplays/roleplay_main.dart
+++ lib/roleplays/roleplay_main.dart
@@ -1,24 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:marquee/marquee.dart';
 
-
-
+import 'package:testing/popular_widgets/drawer.dart';
 import 'package:testing/popular_widgets/appbar_dropdown_button.dart';
 import 'package:testing/popular_widgets/appbar_custom.dart';
 import 'package:testing/popular_widgets/appbar_top_buttons.dart';
 import 'package:testing/popular_widgets/footer.dart';
 import 'package:testing/profile/profile_pics_icons.dart';
 import 'package:testing/profile/profile_tabbar.dart';
-
+import 'package:testing/main_page/marquee.dart';
+import 'package:testing/main_page/roleplay_list_layout.dart';
 
 class roleplay_main extends StatelessWidget {
-  final currentScreen = roleplay_main;
+  final currentScreen = main_page;
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
     Color myColor = Theme.of(context).accentColor;
 
     return Scaffold(
-      body: Text('testd'),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(55),
+        child: appbar_custom(),
+      ),
+      drawer: drawer(currentScreen),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: SingleChildScrollView(
+          physics: ScrollPhysics(),
+          child: Column(
+            children: <Widget>[
+              Container(color: theme.splashColor, height: 4),
+              profile_top_buttons(),
+              Container(color: theme.splashColor, height: 4),
+              Container(
+                color: theme.accentColor,
+                child: marquee(),
+              ),
+              Container(color: theme.splashColor, height: 4),
+              Container(
+                color: theme.backgroundColor,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Popular',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: theme.primaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(color: theme.backgroundColor, child: roleplay_list_layout()),
+              Container(color: theme.splashColor, height: 4),
+              footer(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+/*class profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+  }
+  }*/
