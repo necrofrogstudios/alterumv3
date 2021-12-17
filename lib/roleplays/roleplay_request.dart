4081921c5161f13a199b1d78a12d9100daeedf1c
FlutLab.io <>
2021-12-17 10:26:37 +0000

    Autosave

--- lib/roleplays/roleplay_request.dart
+++ lib/roleplays/roleplay_request.dart
@@ -11,7 +11,23 @@ class roleplay_request extends StatelessWidget {
         splashColor: Colors.transparent,
         highlightColor: Colors.transparent,
         height: 60.0,
-        onPressed: () {},
+        onPressed: () => showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('AlertDialog Title'),
+          content: const Text('AlertDialog description'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      ),,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
