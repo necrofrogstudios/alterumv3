69bc6179f4e3e0f10606a07324edafc627576159
FlutLab.io <>
2021-12-18 00:14:46 +0000

    Autosave

--- lib/profile/tabbar_wall.dart
+++ lib/profile/tabbar_wall.dart
@@ -25,10 +25,8 @@ class _wall_tabState extends State<wall_tab> {
     var theme = Theme.of(context);
     return Container(
       width: double.infinity,
-      child: ListView(
-        scrollDirection: Axis.vertical,
-        shrinkWrap: true,
-        padding: const EdgeInsets.all(0),
+      height: double.infinity,
+      child: Column(
         children: <Widget>[
           Padding(
             padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
