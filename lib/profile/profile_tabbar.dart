f46d281f080e66b05a98df370f49de7b08232960
FlutLab.io <>
2021-12-18 08:01:03 +0000

    Autosave

--- lib/profile/profile_tabbar.dart
+++ lib/profile/profile_tabbar.dart
@@ -30,7 +30,6 @@ class profile_tabbar extends StatelessWidget {
               ),
             ),
             Container(
-              height: 900,
               child: TabBarView(
                 children: <Widget>[
                   wall_tab(),
