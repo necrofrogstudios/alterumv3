5a76416d161d6545d7896397258520d28fda6352
FlutLab.io <>
2021-12-21 10:00:51 +0000

    Autosave

---
+++ lib/roleplays/roleplay_masterlist.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:marquee/marquee.dart';
+
+import 'package:testing/popular_widgets/drawer.dart';
+import 'package:testing/popular_widgets/appbar_dropdown_button.dart';
+import 'package:testing/popular_widgets/appbar_custom.dart';
+import 'package:testing/popular_widgets/appbar_top_buttons.dart';
+import 'package:testing/popular_widgets/footer.dart';
+import 'package:testing/profile/profile_pics_icons.dart';
+import 'package:testing/profile/profile_tabbar.dart';
+import 'package:testing/roleplays/roleplay_request.dart';
+import 'package:testing/roleplays/admin_marquee.dart';
+import 'package:testing/main_page/roleplay_list_layout.dart';
+
+class roleplay_masterlist extends StatelessWidget {
+  final currentScreen = roleplay_rules;
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    Color myColor = Theme.of(context).accentColor;
+
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(55),
+        child: appbar_custom(),
+      ),
+      drawer: drawer(currentScreen),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: SingleChildScrollView(
+          physics: ScrollPhysics(),
+          child: Column(
+            children: <Widget>[
+              Container(color: theme.splashColor, height: 4),
+              profile_top_buttons(),
+              Container(color: theme.splashColor, height: 4),
+              Container(
+                color: theme.accentColor,
+                child: admin_marquee(),
+              ),
+              Container(color: theme.splashColor, height: 4),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      Text('test'),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: theme.backgroundColor,
+                height: 200,
+              ),
+              Container(color: theme.splashColor, height: 4),
+              footer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+/*class profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+  }
+  }*/
