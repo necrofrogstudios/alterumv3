64fb6b7a2890cc55e80d1341fbf075a528b57bb0
FlutLab.io <>
2021-12-21 10:32:27 +0000

    Autosave

--- lib/roleplays/roleplay_masterlist.dart
+++ lib/roleplays/roleplay_masterlist.dart
@@ -46,6 +46,7 @@ class roleplay_masterlist extends StatelessWidget {
                     Container(
                       width: MediaQuery.of(context).size.width,
                       height: 100,
+                      width, 50,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(30),
