3018bb8a823cc3a18db5d986823ee72ee6001130
FlutLab.io <>
2021-12-22 03:15:45 +0000

    Autosave

--- lib/roleplays/roleplay_character_list.dart
+++ lib/roleplays/roleplay_character_list.dart
@@ -83,7 +83,7 @@ class character_johncena extends StatelessWidget {
         children: <Widget>[
           Container(
             width: 100,
-            height: 80,
+            height: 50,
             decoration: BoxDecoration(
               color: theme.backgroundColor,
               borderRadius: BorderRadius.only(
