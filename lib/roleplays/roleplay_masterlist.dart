04336d33dd6aa7f4ae860c0f27200a535c93ec91
FlutLab.io <>
2021-12-22 05:03:11 +0000

    Autosave

--- lib/roleplays/roleplay_masterlist.dart
+++ lib/roleplays/roleplay_masterlist.dart
@@ -61,7 +61,7 @@ class roleplay_masterlist extends StatelessWidget {
                           padding: const EdgeInsets.all(0),
                           crossAxisSpacing: 5,
                           mainAxisSpacing: 5,
-                          crossAxisCount: 3,
+                          crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
                           children: <Widget>[
                             character_parkjimin(),
                             character_kimjaejoong(),
@@ -78,7 +78,6 @@ class roleplay_masterlist extends StatelessWidget {
                             character_kimjaejoong(),
                             character_johncena(),
                           ],
-                          crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
                         );
                       },
                     ),
