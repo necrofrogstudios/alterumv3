e1abfacbd5575f591d7792ab80d59507f6ef1145
FlutLab.io <>
2021-12-22 03:23:43 +0000

    Autosave

--- lib/roleplays/roleplay_character_list.dart
+++ lib/roleplays/roleplay_character_list.dart
@@ -224,39 +224,36 @@ class character_bangyongguk extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    return Padding(
-      padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-      child: Card(
-        color: theme.backgroundColor,
-        child: Column(
-          children: <Widget>[
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: theme.backgroundColor,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                ),
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: NetworkImage("https://th.bing.com/th/id/OIP.zwI_5szHy5ChTmwJhgzIaAHaHa?pid=ImgDet&rs=1"),
-                ),
+    return Card(
+      color: theme.backgroundColor,
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: 100,
+            height: 80,
+            decoration: BoxDecoration(
+              color: theme.backgroundColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: NetworkImage("https://th.bing.com/th/id/OIP.zwI_5szHy5ChTmwJhgzIaAHaHa?pid=ImgDet&rs=1"),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-              child: Text(
-                'Tyler Blackburn',
-                style: TextStyle(
-                  color: theme.primaryColor,
-                  fontSize: 15,
-                ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+            child: Text(
+              'Tyler Blackburn',
+              style: TextStyle(
+                color: theme.primaryColor,
+                fontSize: 15,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -266,39 +263,36 @@ class character_chengjunya extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    return Padding(
-      padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-      child: Card(
-        color: theme.backgroundColor,
-        child: Column(
-          children: <Widget>[
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: theme.backgroundColor,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                ),
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: NetworkImage("https://th.bing.com/th/id/OIP.e32kH9Lro6MOvhZW_NiiogHaHa?pid=ImgDet&rs=1"),
-                ),
+    return Card(
+      color: theme.backgroundColor,
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: 100,
+            height: 80,
+            decoration: BoxDecoration(
+              color: theme.backgroundColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: NetworkImage("https://th.bing.com/th/id/OIP.e32kH9Lro6MOvhZW_NiiogHaHa?pid=ImgDet&rs=1"),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-              child: Text(
-                'Calliope Mori',
-                style: TextStyle(
-                  color: theme.primaryColor,
-                  fontSize: 15,
-                ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+            child: Text(
+              'Calliope Mori',
+              style: TextStyle(
+                color: theme.primaryColor,
+                fontSize: 15,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -308,39 +302,36 @@ class character_parkchanyeol extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    return Padding(
-      padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-      child: Card(
-        color: theme.backgroundColor,
-        child: Column(
-          children: <Widget>[
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: theme.backgroundColor,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                ),
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: NetworkImage("https://th.bing.com/th/id/R.1daa4f1db1545cc864e8733273834ed3?rik=4RIbD%2bf0MyHAdg&riu=http%3a%2f%2f2.bp.blogspot.com%2f-LqRvElpvWqM%2fUCH-NbQtGFI%2fAAAAAAAAGBI%2f-UpYJoCU6L0%2fs1600%2flee-taemin-lee-taemin-23578754-888-789.jpg&ehk=A%2brUdHBOGXIHbHyaCMLTzXjWiwpRBFykrSGYx8WLHAc%3d&risl=&pid=ImgRaw&r=0"),
-                ),
+    return Card(
+      color: theme.backgroundColor,
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: 100,
+            height: 80,
+            decoration: BoxDecoration(
+              color: theme.backgroundColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: NetworkImage("https://th.bing.com/th/id/R.1daa4f1db1545cc864e8733273834ed3?rik=4RIbD%2bf0MyHAdg&riu=http%3a%2f%2f2.bp.blogspot.com%2f-LqRvElpvWqM%2fUCH-NbQtGFI%2fAAAAAAAAGBI%2f-UpYJoCU6L0%2fs1600%2flee-taemin-lee-taemin-23578754-888-789.jpg&ehk=A%2brUdHBOGXIHbHyaCMLTzXjWiwpRBFykrSGYx8WLHAc%3d&risl=&pid=ImgRaw&r=0"),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-              child: Text(
-                'Lee Taemin',
-                style: TextStyle(
-                  color: theme.primaryColor,
-                  fontSize: 15,
-                ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+            child: Text(
+              'Lee Taemin',
+              style: TextStyle(
+                color: theme.primaryColor,
+                fontSize: 15,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
