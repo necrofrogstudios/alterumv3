51c6f32430713b984de56baff7eb052c3a2f753d
FlutLab.io <>
2021-12-22 02:59:02 +0000

    Autosave

--- lib/roleplays/roleplay_masterlist.dart
+++ lib/roleplays/roleplay_masterlist.dart
@@ -49,7 +49,7 @@ class roleplay_masterlist extends StatelessWidget {
                   padding: const EdgeInsets.all(20),
                   crossAxisSpacing: 10,
                   mainAxisSpacing: 10,
-                  crossAxisCount: 3,
+                  crossAxisCount: 2,
                   children: <Widget>[
                     Container(
                       padding: const EdgeInsets.all(8),
