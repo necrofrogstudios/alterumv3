5c21c3545df7bd9b859fcaed2f878ca01cf89411
FlutLab.io <>
2021-12-22 11:03:26 +0000

    Autosave

--- lib/roleplays/roleplay_character.dart
+++ lib/roleplays/roleplay_character.dart
@@ -55,10 +55,7 @@ class roleplay_character extends StatelessWidget {
                     bottomRight: Radius.circular(30),
                   ),
                 ),
-              ),
-              child: FittedBox(
-                child: Image.network('https://th.bing.com/th/id/OIP.-o2GCLO_A2unfT5yubh7HwHaHa?pid=ImgDet&rs=1'),
-                fit: BoxFit.fill,
+                child: Text('test'),
               ),
             ),
             Container(height: 400, color: theme.backgroundColor),
