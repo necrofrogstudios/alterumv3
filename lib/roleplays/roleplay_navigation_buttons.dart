06936c9b1a2fa70a99b7fec43f0d025876cf87f1
FlutLab.io <>
2021-12-24 02:27:21 +0000

    Autosave

--- lib/roleplays/roleplay_navigation_buttons.dart
+++ lib/roleplays/roleplay_navigation_buttons.dart
@@ -35,7 +35,10 @@ class roleplay_navigation_buttons extends StatelessWidget {
                 style: TextStyle(fontSize: 12, color: theme.primaryColor),
               ),
               onPressed: () {
-                MaterialPageRoute(builder: (context) => navigation[i]);
+                Navigator.pop(
+                  context,
+                  MaterialPageRoute(builder: (context) => navigation[i])),
+                );
               },
             ),
           ),
