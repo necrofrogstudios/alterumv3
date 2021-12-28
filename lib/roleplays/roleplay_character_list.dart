6a506a243ae8593ed20248394d0ab1932c04e325
FlutLab.io <>
2021-12-28 06:49:10 +0000

    Autosave

--- lib/roleplays/roleplay_character_list.dart
+++ lib/roleplays/roleplay_character_list.dart
@@ -60,7 +60,7 @@ class character_kimjaejoong extends StatelessWidget {
                             decoration: BoxDecoration(
                               color: theme.accentColor,
                               borderRadius: BorderRadius.only(
-                                bottomRight: Radius.circular(10),
+                                bottomRight: Radius.circular(5),
                               ),
                             ),
                             child: Padding(
