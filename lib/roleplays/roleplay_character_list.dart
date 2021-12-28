9df2f95fd2a00ffc233db938cbe8dfeecbb238da
FlutLab.io <>
2021-12-28 06:33:42 +0000

    Autosave

--- lib/roleplays/roleplay_character_list.dart
+++ lib/roleplays/roleplay_character_list.dart
@@ -54,16 +54,19 @@ class character_kimjaejoong extends StatelessWidget {
                       child: FractionallySizedBox(
                         widthFactor: 1,
                         heightFactor: 0.2,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: theme.accentColor,
-                          ),
-                          child: Padding(
-                            padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                            child: Text(
-                              ' Park Jimin',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(color: theme.primaryColor, fontSize: 15),
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(0, 100, 0, 20),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: theme.accentColor,
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                              child: Text(
+                                ' Park Jimin',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: theme.primaryColor, fontSize: 15),
+                              ),
                             ),
                           ),
                         ),
