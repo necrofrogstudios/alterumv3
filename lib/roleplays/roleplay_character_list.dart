97972372562cb381c08908a81c78138ecc3f32e3
FlutLab.io <>
2021-12-28 03:08:27 +0000

    Autosave

--- lib/roleplays/roleplay_character_list.dart
+++ lib/roleplays/roleplay_character_list.dart
@@ -109,7 +109,7 @@ class character_kimjaejoong extends StatelessWidget {
                       highlightColor: Colors.transparent,
                       onPressed: () {},
                       child: 
-                    ),
+                    
                   ),
                 ],
               ),
