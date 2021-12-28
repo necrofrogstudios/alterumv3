fcfc881629b11777785253ef056ece4f867ecbc2
FlutLab.io <>
2021-12-28 02:30:02 +0000

    Autosave

--- lib/roleplays/roleplay_character_list.dart
+++ lib/roleplays/roleplay_character_list.dart
@@ -7,18 +7,15 @@ class character_parkjimin extends StatelessWidget {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
-        Container(
-          width: 100,
-          height: 80,
-          decoration: BoxDecoration(
-            color: theme.backgroundColor,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30),
-            ),
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: NetworkImage("https://th.bing.com/th/id/OIP.-o2GCLO_A2unfT5yubh7HwHaHa?pid=ImgDet&rs=1"),
+        Card(
+          shape: RoundedRectangleBorder(
+            side: BorderSide(color: theme.splashColor, width: 4),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30.0),
+            child: FittedBox(
+              child: Image.network('https://th.bing.com/th/id/OIP.-o2GCLO_A2unfT5yubh7HwHaHa?pid=ImgDet&rs=1'),
             ),
           ),
         ),
