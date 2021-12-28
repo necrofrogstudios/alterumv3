f6bf6437c3b8454a61bf0ec49e5229f2aa79cf8f
FlutLab.io <>
2021-12-28 07:06:16 +0000

    Autosave

--- lib/roleplays/roleplay_masterlist.dart
+++ lib/roleplays/roleplay_masterlist.dart
@@ -63,7 +63,7 @@ class roleplay_masterlist extends StatelessWidget {
                           padding: const EdgeInsets.all(5),
                           crossAxisSpacing: 5,
                           mainAxisSpacing: 5,
-                          crossAxisCount: orientation == Orientation.portrait ? 3 : 5,
+                          crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
                           children: <Widget>[
                             GestureDetector(
                               onTap: () {
