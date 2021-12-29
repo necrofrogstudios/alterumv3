d12c0e597099d1efe2d206bfc493899a575d583f
FlutLab.io <>
2021-12-29 10:07:47 +0000

    Autosave

--- lib/settings/themes_navigation.dart
+++ lib/settings/themes_navigation.dart
@@ -58,7 +58,7 @@ class Card2 extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              themes(),
+              MultipleThemesView(),
             ],
           ),
         ),
