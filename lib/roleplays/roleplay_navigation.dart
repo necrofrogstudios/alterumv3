cdf354a7bb0460c286ca198038c82907f29e2819
FlutLab.io <>
2021-12-29 10:28:55 +0000

    Autosave

--- lib/roleplays/roleplay_navigation.dart
+++ lib/roleplays/roleplay_navigation.dart
@@ -51,7 +51,6 @@ class Card2 extends StatelessWidget {
         constraints: BoxConstraints(
           maxWidth: 2500.0,
           minWidth: 1450.0,
-          shrinkWrap: true,
         ),
         child: Padding(
           padding: EdgeInsets.all(10),
