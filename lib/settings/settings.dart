2264e9d18431c0876f37ce04b8bdb1402988fc96
FlutLab.io <>
2021-12-29 09:49:36 +0000

    Autosave

--- lib/settings/settings.dart
+++ lib/settings/settings.dart
@@ -40,7 +40,7 @@ class _settingsState extends State<settings> {
           physics: ScrollPhysics(),
           child: Column(
             children: <Widget>[
-              Container(color: theme.backgroundColor, height: 4),
+              Container(color: theme.accentColor, height: 4),
               themes(),
             ],
           ),
