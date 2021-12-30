6cf4b1e800c037265da28680cdd21951bba9f8d2
FlutLab.io <>
2021-12-30 10:34:56 +0000

    Autosave

--- lib/solo/solo_main.dart
+++ lib/solo/solo_main.dart
@@ -69,7 +69,7 @@ class solo_mainState extends State<solo_main> {
               child: Column(
                 children: [
                   Container(
-                    height: 400,
+                    height: 500,
                     child: SwipeCards(
                       matchEngine: _matchEngine,
                       itemBuilder: (BuildContext context, int index) {
