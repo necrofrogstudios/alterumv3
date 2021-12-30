91623951682787dddad8c1abbd7ec09fdc9f5b4d
FlutLab.io <>
2021-12-30 07:15:06 +0000

    Autosave

--- lib/main_page/main_page.dart
+++ lib/main_page/main_page.dart
@@ -31,7 +31,7 @@ class main_page extends StatelessWidget {
           shrinkWrap: true,
           padding: const EdgeInsets.all(0),
           children: <Widget>[
-            Container(color: theme.splashColor, height: 4),
+            Container(color: theme.splashColor, height: 5),
             profile_top_buttons(),
             Container(color: theme.splashColor, height: 4),
             Container(
