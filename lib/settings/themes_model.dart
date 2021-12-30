f1dd9ffe4034f1562d242e01241e6ebc65469f30
FlutLab.io <>
2021-12-30 03:43:11 +0000

    Autosave

--- lib/settings/themes_model.dart
+++ lib/settings/themes_model.dart
@@ -12,7 +12,7 @@ class ThemeModel {
 
 class MultipleThemesViewModel extends BaseViewModel {
   List<ThemeModel> get themes => List<ThemeModel>.generate(
-        27,
+        31,
         (index) => ThemeModel(
           index: index,
           title: _getTitleForIndex(index),
