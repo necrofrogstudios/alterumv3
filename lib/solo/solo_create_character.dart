ebabe3db467df4b96d467e2cc5e9ac090644797a
FlutLab.io <>
2021-12-31 03:20:13 +0000

    Autosave

--- lib/solo/solo_create_character.dart
+++ lib/solo/solo_create_character.dart
@@ -7,14 +7,6 @@ import 'package:testing/popular_widgets/drawer.dart';
 import 'package:testing/popular_widgets/appbar_top_buttons.dart';
 import 'package:testing/solo/solo_navigation.dart';
 
-class Content {
-  final String text;
-  final String text2;
-  final Image child;
-
-  Content({this.text, this.child, this.text2});
-}
-
 class solo_create_character extends StatefulWidget {
   solo_create_character({Key key, this.title}) : super(key: key);
 
@@ -27,30 +19,6 @@ class solo_create_character extends StatefulWidget {
 class solo_create_characterState extends State<solo_create_character> {
   final currentScreen = solo_create_character;
 
-  List<SwipeItem> _swipeItems = <SwipeItem>[];
-  MatchEngine _matchEngine;
-  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
-  List<String> names = [
-    "Park Jimin",
-    "Kim Jaejoong",
-    "John Cena",
-    "Kai",
-    "Tyler Blackburn"
-  ];
-  List<Image> images = [
-    Image.network('https://th.bing.com/th/id/OIP._qHh64G9n7v_I4ThR3dEngHaHa?pid=ImgDet&rs=1'),
-    Image.network('https://th.bing.com/th/id/OIP.6Nepn0j46UEPZAI1S-n7cAHaHa?pid=ImgDet&rs=1'),
-    Image.network('https://www.thefamouspeople.com/profiles/images/john-cena-8.jpg'),
-    Image.network('https://images.genius.com/55b1c5a277d7bd9b124027f96085e3f2.1000x1000x1.jpg'),
-    Image.network('https://th.bing.com/th/id/OIP.5hmPeemCMT2wyhElgFqMogHaHa?pid=ImgDet&rs=1'),
-  ];
-  List<String> info = [
-    "Park Jiminz",
-    "Kim Jaejoong",
-    "John Cena",
-    "Kai",
-    "Tyler Blackburn"
-  ];
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
---
+++ lib/solo/solo_create_character_input_box.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class solo_create_character_input_box extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {}
+}
