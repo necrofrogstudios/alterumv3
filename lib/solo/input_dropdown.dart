e4f15baa1dc2e7d4d999054c327be3010ab428ee
FlutLab.io <>
2021-12-31 07:03:08 +0000

    Autosave

--- lib/solo/input_dropdown.dart
+++ lib/solo/input_dropdown.dart
@@ -8,7 +8,7 @@ class input_dropdown extends StatefulWidget {
 class input_dropdownState extends State<input_dropdown> {
   List<int> ages = [];
 
-  int _chosenValue = 18;
+  String _chosenValue = '18';
 
   @override
   Widget build(BuildContext context) {
