46f8266627777184b9a8d30feb60ad29791d75b7
FlutLab.io <>
2021-12-31 05:56:23 +0000

    Autosave

--- lib/solo/input_dropdown.dart
+++ lib/solo/input_dropdown.dart
@@ -18,7 +18,7 @@ class input_dropdownState extends State<input_dropdown> {
     var theme = Theme.of(context);
     return Center(
       child: Container(
-        constraints: BoxConstraints(minWidth: 300, maxWidth: 900),
+        width: MediaQuery.of(context).size.width * 0.5,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.all(Radius.circular(30)),
           border: Border.all(
