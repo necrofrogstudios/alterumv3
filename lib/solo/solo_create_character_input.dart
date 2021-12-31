59d1770d1e0bafd7dca03f42fa929bd896a90ce6
FlutLab.io <>
2021-12-31 03:53:05 +0000

    Autosave

--- lib/solo/solo_create_character_input.dart
+++ lib/solo/solo_create_character_input.dart
@@ -4,28 +4,32 @@ class solo_create_character_input extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    return Padding(
-      padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-      child: TextFormField(
-        style: TextStyle(color: theme.primaryColor),
-        keyboardType: TextInputType.multiline,
-        maxLines: null,
-        decoration: InputDecoration(
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(50.0)),
-            borderSide: BorderSide(color: theme.accentColor, width: 4.0),
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: TextFormField(
+            style: TextStyle(color: theme.primaryColor),
+            keyboardType: TextInputType.multiline,
+            maxLines: null,
+            decoration: InputDecoration(
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                borderSide: BorderSide(color: theme.accentColor, width: 4.0),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                borderSide: BorderSide(color: theme.accentColor, width: 4.0),
+              ),
+              hintText: 'Type Here',
+              hintStyle: TextStyle(
+                color: theme.primaryColor.withOpacity(0.45),
+              ),
+              contentPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20),
+            ),
           ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(50.0)),
-            borderSide: BorderSide(color: theme.accentColor, width: 4.0),
-          ),
-          hintText: 'Type Here',
-          hintStyle: TextStyle(
-            color: theme.primaryColor.withOpacity(0.45),
-          ),
-          contentPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20),
         ),
-      ),
+      ],
     );
   }
 }
