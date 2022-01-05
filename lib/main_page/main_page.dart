495fb8d6456e7ba1b4dacc7f5aba440322758611
FlutLab.io <>
2022-01-05 12:28:24 +0000

    Autosave

--- lib/main_page/main_page.dart
+++ lib/main_page/main_page.dart
@@ -28,6 +28,15 @@ class main_pageState extends State<main_page> {
     _refreshController.refreshCompleted();
   }
 
+  void _onLoading() async {
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use loadFailed(),if no data return,use LoadNodata()
+    items.add((items.length + 1).toString());
+    if (mounted) setState(() {});
+    _refreshController.loadComplete();
+  }
+
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
