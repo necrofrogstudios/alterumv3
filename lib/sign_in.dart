7b46c83c31d5786bbe7b5005201a37c256de3381
FlutLab.io <>
2022-01-05 18:19:09 +0000

    Autosave

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class sign_in extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container (color:theme.backgroundColor, height:500)
-
+    return Container(color: theme.backgroundColor, height: 500);
   }
-  }
+}
