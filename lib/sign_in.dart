0be966cf68715f57789a20c515cc93f0e6c972a1
FlutLab.io <>
2022-01-05 18:36:06 +0000

    Autosave

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/testing/profile/profile.dart';
 
 class sign_in extends StatelessWidget {
   @override
@@ -18,6 +19,37 @@ class sign_in extends StatelessWidget {
               style: TextStyle(color: theme.primaryColor, fontSize: 20),
             ),
             Container(height: 100, width: 100, color: theme.accentColor),
+            FlatButton(
+              color: theme.accentColor,
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              height: 60.0,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => profile()),
+                );
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Profile',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: theme.primaryColor,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.arrow_forward,
+                      color: theme.primaryColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
