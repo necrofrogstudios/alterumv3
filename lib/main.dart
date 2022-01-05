d9bb89c0a1c6d69101a6923b7271afecf61d81ec
FlutLab.io <>
2022-01-05 12:02:06 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+Firebase-import 'package:flutter/material.dart';
 import 'package:flutter_launcher_icons/android.dart';
 import 'package:flutter_launcher_icons/constants.dart';
 import 'package:flutter_launcher_icons/custom_exceptions.dart';
@@ -6,6 +6,7 @@ import 'package:flutter_launcher_icons/ios.dart';
 import 'package:flutter_launcher_icons/main.dart';
 import 'package:flutter_launcher_icons/utils.dart';
 import 'package:flutter_launcher_icons/xml_templates.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 import 'package:get_it/get_it.dart';
 import 'package:stacked_themes/stacked_themes.dart';
@@ -21,11 +22,21 @@ Future main() async {
   setupLocator();
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ThemeBuilder(
+    return RefreshConfiguration(
+        headerBuilder: () => WaterDropMaterialHeader(), // Configure the default header indicator. If you have the same header indicator for each page, you need to set this
+        footerBuilder: () => ClassicFooter(), // Configure default bottom indicator
+        headerTriggerDistance: 80.0, // header trigger refresh trigger distance
+        // custom spring back animate,the props meaning see the flutter api
+        maxOverScrollExtent: 200, //The maximum dragging range of the head. Set this property if a rush out of the view area occurs
+        maxUnderScrollExtent: 0, // Maximum dragging range at the bottom
+        enableScrollWhenRefreshCompleted: true, //This property is incompatible with PageView and TabBarView. If you need TabBarView to slide left and right, you need to set it to true.
+        enableLoadingWhenFailed: true, //In the case of load failure, users can still trigger more loads by gesture pull-up.
+        hideFooterWhenNotFull: false, // Disable pull-up to load more functionality when Viewport is less than one screen
+        enableBallisticLoad: true, // trigger load more by BallisticScrollActivity
+        child: ThemeBuilder(
       themes: getThemes,
       builder: (context, regularTheme, darkTheme, themeMode) => MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -35,9 +46,11 @@ class MyApp extends StatelessWidget {
         themeMode: themeMode,
         home: new Splash(),
       ),
-    );
+    ),);
   }
 }
+
+
 
 final GetIt locator = GetIt.instance;
 
