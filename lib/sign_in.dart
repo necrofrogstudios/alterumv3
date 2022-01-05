92b16b661a6db68de8cb52b2c199d8d906fe7282
FlutLab.io <>
2022-01-05 18:27:16 +0000

    Autosave

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -4,6 +4,7 @@ class sign_in extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    return Container(color: theme.primaryColor, height: 500);
+    return Container(color: theme.backgroundColor, height: 500,
+    child:Column(children: <Widget>[Text('blah'style:TextStyle(color: theme.primaryColor),),],),);
   }
 }
