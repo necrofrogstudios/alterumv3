9073db7ef869a923314d036fe105bb6004e1b684
FlutLab.io <>
2022-01-05 12:29:04 +0000

    Autosave

--- lib/main_page/main_page.dart
+++ lib/main_page/main_page.dart
@@ -32,7 +32,7 @@ class main_pageState extends State<main_page> {
     // monitor network fetch
     await Future.delayed(Duration(milliseconds: 1000));
     // if failed,use loadFailed(),if no data return,use LoadNodata()
-    items.add((items.length + 1).toString());
+
     if (mounted) setState(() {});
     _refreshController.loadComplete();
   }
