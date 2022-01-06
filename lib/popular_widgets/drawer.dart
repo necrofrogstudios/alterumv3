d9fffeb22a05a639d2c73450d36ab5b280d28606
FlutLab.io <>
2022-01-06 03:33:52 +0000

    Autosave

--- lib/popular_widgets/drawer.dart
+++ lib/popular_widgets/drawer.dart
@@ -50,6 +50,7 @@ class drawer extends StatelessWidget {
             ),
             Container(color: theme.splashColor, height: 4),
             Container(
+              height: 200,
               color: theme.backgroundColor,
               child: Padding(
                 padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
