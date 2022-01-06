d0fec2e88fd90bd158dce29a617f8c89fd9e664c
FlutLab.io <>
2022-01-06 04:58:44 +0000

    Autosave

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -6,65 +6,61 @@ class sign_in extends StatelessWidget {
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: theme.backgroundColor,
-        title: Container(
-          height: 40,
-          color: theme.backgroundColor,
-        ),
-      ),
-      body: Container(
-        height: 500,
-        width: double.infinity,
-        constraints: BoxConstraints.expand(),
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage("https://wallpaperaccess.com/full/1465993.jpg"),
-            fit: BoxFit.cover,
+      body: Padding(
+        padding: EdgeInsets.only(top: 40),
+        child: Container(
+          height: 500,
+          width: double.infinity,
+          constraints: BoxConstraints.expand(),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage("https://wallpaperaccess.com/full/1465993.jpg"),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              child: Text(
-                'Sign In',
-                textAlign: TextAlign.center,
-                style: TextStyle(color: theme.primaryColor, fontSize: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: Text(
+                  'Sign In',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: theme.primaryColor, fontSize: 20),
+                ),
               ),
-            ),
-            Image.network("https://cdn.iconscout.com/icon/free/png-256/theater-mask-33-1184374.png"),
-            OutlineButton(
-              color: theme.accentColor,
-              splashColor: Colors.transparent,
-              highlightColor: Colors.transparent,
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => profile()),
-                );
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Profile',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: theme.primaryColor,
+              Image.network("https://cdn.iconscout.com/icon/free/png-256/theater-mask-33-1184374.png"),
+              OutlineButton(
+                color: theme.accentColor,
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => profile()),
+                  );
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Profile',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: theme.primaryColor,
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Icon(
-                      Icons.arrow_forward,
-                      color: theme.primaryColor,
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(
+                        Icons.arrow_forward,
+                        color: theme.primaryColor,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
