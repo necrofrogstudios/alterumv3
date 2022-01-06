6c9a74c661cf429df44146ce7b1e5762907168a0
FlutLab.io <>
2022-01-06 05:19:00 +0000

    Autosave

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -6,70 +6,67 @@ class sign_in extends StatelessWidget {
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
     return Scaffold(
-      body: ListView(
-        shrinkWrap: true,
-        child: Container(
-          height: 500,
-          width: double.infinity,
-          constraints: BoxConstraints.expand(),
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage("https://64.media.tumblr.com/9b7ac6e7d24cf994c538d166ff330f53/e05d144f651b2db4-a6/s400x600/50ed90a10be694dfd4203ec45627f5c1f355c69e.jpg"),
-              fit: BoxFit.cover,
-            ),
+      body: Container(
+        height: 500,
+        width: double.infinity,
+        constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage("https://64.media.tumblr.com/9b7ac6e7d24cf994c538d166ff330f53/e05d144f651b2db4-a6/s400x600/50ed90a10be694dfd4203ec45627f5c1f355c69e.jpg"),
+            fit: BoxFit.cover,
           ),
-          child: Padding(
-            padding: EdgeInsets.only(top: 40),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  child: Text(
-                    'Alterum',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),
-                  ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(top: 40),
+          child: ListView(
+            shrinkWrap: true,
+            children: <Widget>[
+              Container(
+                child: Text(
+                  'Alterum',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),
                 ),
-                Image.network("https://cdn.iconscout.com/icon/free/png-256/theater-mask-33-1184374.png"),
-                Container(
-                  child: Text(
-                    'Sign In',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: theme.primaryColor, fontSize: 20),
-                  ),
+              ),
+              Image.network("https://cdn.iconscout.com/icon/free/png-256/theater-mask-33-1184374.png"),
+              Container(
+                child: Text(
+                  'Sign In',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: theme.primaryColor, fontSize: 20),
                 ),
-                OutlineButton(
-                  color: theme.accentColor,
-                  splashColor: Colors.transparent,
-                  highlightColor: Colors.transparent,
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => profile()),
-                    );
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Profile',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: theme.primaryColor,
-                        ),
+              ),
+              OutlineButton(
+                color: theme.accentColor,
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => profile()),
+                  );
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Profile',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: theme.primaryColor,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Icon(
-                          Icons.arrow_forward,
-                          color: theme.primaryColor,
-                        ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(
+                        Icons.arrow_forward,
+                        color: theme.primaryColor,
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
