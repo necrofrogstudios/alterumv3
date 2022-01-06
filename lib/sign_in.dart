0448388e955a70b5c150b3915f425d582adc6607
FlutLab.io <>
2022-01-06 05:39:25 +0000

    Autosave

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -82,27 +82,24 @@ class input_username extends StatelessWidget {
           topRight: Radius.circular(40),
         ),
       ),
-      child: Padding(
-        padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-        child: TextFormField(
-          style: TextStyle(color: theme.primaryColor),
-          keyboardType: TextInputType.multiline,
-          maxLines: null,
-          decoration: InputDecoration(
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(50.0)),
-              borderSide: BorderSide(color: Colors.yellow, width: 4.0),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(50.0)),
-              borderSide: BorderSide(color: Colors.yellow, width: 4.0),
-            ),
-            hintText: 'Username',
-            hintStyle: TextStyle(
-              color: theme.primaryColor.withOpacity(0.75),
-            ),
-            contentPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20),
+      child: TextFormField(
+        style: TextStyle(color: theme.primaryColor),
+        keyboardType: TextInputType.multiline,
+        maxLines: null,
+        decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(50.0)),
+            borderSide: BorderSide(color: Colors.yellow, width: 4.0),
           ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(50.0)),
+            borderSide: BorderSide(color: Colors.yellow, width: 4.0),
+          ),
+          hintText: 'Username',
+          hintStyle: TextStyle(
+            color: theme.primaryColor.withOpacity(0.75),
+          ),
+          contentPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20),
         ),
       ),
     );
