6195b51a690a2f19e3a54d8f9639181e6c0afea1
FlutLab.io <>
2022-01-07 08:13:23 +0000

    Autosave

--- lib/sign_in/forgotten_sign_in.dart
+++ lib/sign_in/forgotten_sign_in.dart
@@ -25,7 +25,7 @@ class forgotten_sign_inState extends State<forgotten_sign_in> {
           ),
         ),
         child: Padding(
-          padding: EdgeInsets.only(top: 40),
+          padding: EdgeInsets.only(top: 30),
           child: ListView(
             shrinkWrap: true,
             children: <Widget>[
