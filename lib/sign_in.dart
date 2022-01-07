9dea6cee828179d6e139a67c75e58fd8227247b4
FlutLab.io <>
2022-01-07 01:38:00 +0000

    Autosave

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -29,7 +29,11 @@ class sign_inState extends State<sign_in> {
           child: ListView(
             shrinkWrap: true,
             children: <Widget>[
-              Icon(Icons.headset_mic, size: 30, color: Colors.white),
+              Row(
+                children: [
+                  Icon(Icons.headset_mic, size: 30, color: Colors.white),
+                ],
+              ),
               Container(
                 child: Text(
                   'Alterum',
