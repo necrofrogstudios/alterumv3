12124e3b60ae96aae2e9fc598d62e1de4790f7f4
FlutLab.io <>
2022-01-07 05:38:38 +0000

    Autosave

--- lib/sign_in/sign_in.dart
+++ lib/sign_in/sign_in.dart
@@ -33,7 +33,7 @@ class sign_inState extends State<sign_in> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Padding(
-                    padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                    padding: EdgeInsets.fromLTRB(0, 0, 25, 0),
                     child: IconButton(
                       icon: Icon(Icons.headset_mic, size: 30, color: Colors.white),
                       tooltip: 'Increase volume by 10',
