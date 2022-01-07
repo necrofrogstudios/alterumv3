6710ddd5a617572787f159f3738ce3a04335bd10
FlutLab.io <>
2022-01-07 02:13:47 +0000

    Autosave

--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -32,7 +32,7 @@ class sign_upState extends State<sign_up> {
               Row(
                 children: [
                   IconButton(
-                    icon: const Icon(Icons.arrow_back),
+                    icon: const Icon(Icons.arrow_back, color: Colors.white, size: 30),
                     onPressed: () {},
                   ),
                 ],
