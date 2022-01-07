9e6f17da011162dc1066faeb2144005c65234a50
FlutLab.io <>
2022-01-07 06:28:57 +0000

    Autosave

--- lib/support/contact_support.dart
+++ lib/support/contact_support.dart
@@ -117,34 +117,31 @@ class input_complaint extends StatelessWidget {
     var theme = Theme.of(context);
     return Padding(
       padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-      child: SizedBox(
-        height: 300,
-        child: Container(
-          decoration: BoxDecoration(
-            color: theme.accentColor,
-            borderRadius: BorderRadius.all(
-              Radius.circular(50),
-            ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: theme.accentColor,
+          borderRadius: BorderRadius.all(
+            Radius.circular(50),
           ),
-          child: TextFormField(
-            style: TextStyle(color: theme.primaryColor),
-            keyboardType: TextInputType.multiline,
-            maxLines: null,
-            decoration: InputDecoration(
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                borderSide: BorderSide(color: theme.splashColor, width: 4.0),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                borderSide: BorderSide(color: theme.splashColor, width: 4.0),
-              ),
-              hintText: 'Enter Email Address',
-              hintStyle: TextStyle(
-                color: theme.primaryColor.withOpacity(0.55),
-              ),
-              contentPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20),
+        ),
+        child: TextFormField(
+          style: TextStyle(color: theme.primaryColor),
+          keyboardType: TextInputType.multiline,
+          maxLines: null,
+          decoration: InputDecoration(
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(50.0)),
+              borderSide: BorderSide(color: theme.splashColor, width: 4.0),
             ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(50.0)),
+              borderSide: BorderSide(color: theme.splashColor, width: 4.0),
+            ),
+            hintText: 'Enter Email Address',
+            hintStyle: TextStyle(
+              color: theme.primaryColor.withOpacity(0.55),
+            ),
+            contentPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20),
           ),
         ),
       ),
