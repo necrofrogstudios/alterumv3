b639c2a5778657a2088c707604d4ddc92922b65c
FlutLab.io <>
2022-01-07 05:04:32 +0000

    Autosave

--- lib/profile/profile_flip_card.dart
+++ lib/profile/profile_flip_card.dart
@@ -32,43 +32,22 @@ class profile_flip_card extends StatelessWidget {
               ),
             ),
           ), //back of card  V V V V //
-          back: Padding(
-            padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
-            child: Container(
-              decoration: BoxDecoration(
-                color: theme.accentColor,
-                border: Border.all(color: theme.splashColor, width: 3.0),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(30),
+          back: Center(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(0, 90, 0, 0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(
+                    color: theme.splashColor,
+                    width: 4,
+                  ),
                 ),
-              ),
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    child: Text(
-                      ' Park Jimin',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: theme.primaryColor, fontSize: 15),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    child: Text(
-                      'activity p',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: theme.primaryColor, fontSize: 15),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    child: Text(
-                      'another',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: theme.primaryColor, fontSize: 15),
-                    ),
-                  ),
-                ],
+                height: 200,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(27.0),
+                  child: Image.network('https://simg.nicepng.com/png/small/8-87422_alien-comments-alien-avatar-red.png'),
+                ),
               ),
             ),
           ),
