ac4303d3b033581abe8a29499253984154034c2a
FlutLab.io <>
2022-01-07 06:52:00 +0000

    Autosave

--- lib/support/contact_support.dart
+++ lib/support/contact_support.dart
@@ -99,7 +99,7 @@ class input_title extends StatelessWidget {
               borderRadius: BorderRadius.all(Radius.circular(50.0)),
               borderSide: BorderSide(color: theme.splashColor, width: 4.0),
             ),
-            hintText: 'Enter Email Address',
+            hintText: 'Subject Matter',
             hintStyle: TextStyle(
               color: theme.primaryColor.withOpacity(0.55),
             ),
@@ -138,7 +138,7 @@ class input_complaint extends StatelessWidget {
               borderRadius: BorderRadius.all(Radius.circular(50.0)),
               borderSide: BorderSide(color: theme.splashColor, width: 4.0),
             ),
-            hintText: 'Subject Matter',
+            hintText: 'Complaint',
             hintStyle: TextStyle(
               color: theme.primaryColor.withOpacity(0.55),
             ),
