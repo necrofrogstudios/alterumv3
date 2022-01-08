d17aaa526702bf3aa23e3b8184efd9f10c88f127
FlutLab.io <>
2022-01-08 11:47:57 +0000

    Autosave

--- lib/popular_widgets/appbar_custom.dart
+++ lib/popular_widgets/appbar_custom.dart
@@ -10,6 +10,12 @@ import 'package:testing/messages/messages_main.dart';
 import 'package:testing/main_page/main_page.dart';
 
 class appbar_custom extends StatelessWidget {
+  final List<Widget> navigation = [
+    main_page(),
+    messages_main(),
+    main_page(),
+    messages_main(),
+  ];
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
 
@@ -45,7 +51,7 @@ class appbar_custom extends StatelessWidget {
             onChange: (index) {
               print(index);
 
-              Navigator.push(context, MaterialPageRoute(builder: (index) => main_page()));
+              Navigator.push(context, MaterialPageRoute(builder: (index) => navigation[index]));
             },
           ),
         ),
