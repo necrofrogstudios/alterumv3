115a32deff9912077bbeb18dc31e0d5875eb874e
FlutLab.io <>
2022-01-08 11:07:23 +0000

    Autosave

--- lib/popular_widgets/appbar_custom.dart
+++ lib/popular_widgets/appbar_custom.dart
@@ -40,8 +40,6 @@ class appbar_custom extends StatelessWidget {
                 ),
                 color: theme.primaryColor,
                 onPressed: () {
-                  closeMenu();
-                  //
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => messages_main()),
