3aec236f603ae718b40008cd8030c8de793d52d2
FlutLab.io <>
2022-01-08 07:36:43 +0000

    Autosave

--- lib/sign_in/sign_in.dart
+++ lib/sign_in/sign_in.dart
@@ -81,7 +81,7 @@ class sign_inState extends State<sign_in> {
               input_username(),
               input_password(),
               Padding(
-                padding: EdgeInsets.fromLTRB(20, 0, 30, 10),
+                padding: EdgeInsets.fromLTRB(20, 0, 20, 10),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
