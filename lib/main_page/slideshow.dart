5313caa4a1f8dbbfd851118f4577e96ac1777933
FlutLab.io <>
2022-01-11 10:08:34 +0000

    Autosave

--- lib/main_page/slideshow.dart
+++ lib/main_page/slideshow.dart
@@ -123,7 +123,7 @@ class slideshow extends StatelessWidget {
 
       /// Auto scroll interval.
       /// Do not auto scroll with null or 0.
-      autoPlayInterval: 100000,
+      autoPlayInterval: 8000,
 
       isLoop: true,
     );
