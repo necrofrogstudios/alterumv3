d1b11d051902c719181478403b7dfe3a7357f846
FlutLab.io <>
2022-01-11 03:26:35 +0000

    Autosave

--- lib/roleplays/create_roleplay_toggles.dart
+++ lib/roleplays/create_roleplay_toggles.dart
@@ -94,3 +94,92 @@ class _pov_toggleState extends State<pov_toggle> {
     );
   }
 }
+
+class mature_toggle extends StatefulWidget {
+  @override
+  _mature_toggleState createState() => _mature_toggleState();
+}
+
+class _mature_toggleState extends State<mature_toggle> {
+  List<bool> isSelected = [
+    true,
+    false,
+  ];
+  FocusNode focusNodeButton1 = FocusNode();
+  FocusNode focusNodeButton2 = FocusNode();
+
+  List<FocusNode> focusToggle;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    focusToggle = [
+      focusNodeButton1,
+      focusNodeButton2,
+    ];
+  }
+
+  @override
+  void dispose() {
+    // Clean up the focus node when the Form is disposed.
+    focusNodeButton1.dispose();
+    focusNodeButton2.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    return Container(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ToggleButtons(
+              color: theme.primaryColor,
+              selectedColor: theme.primaryColor,
+              fillColor: theme.accentColor,
+              splashColor: theme.accentColor,
+              highlightColor: theme.accentColor,
+              borderColor: theme.splashColor,
+              borderWidth: 3,
+              selectedBorderColor: theme.splashColor,
+              renderBorder: true,
+              borderRadius: BorderRadius.all(
+                Radius.circular(50),
+              ),
+              disabledColor: Colors.blueGrey,
+              disabledBorderColor: Colors.blueGrey,
+              focusColor: Colors.red,
+              focusNodes: focusToggle,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Text('First Pov'),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Text('Third Pov'),
+                ),
+              ],
+              isSelected: isSelected,
+              onPressed: (int index) {
+                setState(() {
+                  for (int indexBtn = 0; indexBtn < isSelected.length; indexBtn++) {
+                    if (indexBtn == index) {
+                      isSelected[indexBtn] = true;
+                    } else {
+                      isSelected[indexBtn] = false;
+                    }
+                  }
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
