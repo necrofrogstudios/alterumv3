f81b85c86718506aad3248bcc308fff06bd62593
FlutLab.io <>
2022-01-11 09:21:25 +0000

    Autosave

--- lib/main_page/main_page.dart
+++ lib/main_page/main_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:marquee/marquee.dart';
 import 'package:testing/main.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 
 import 'package:testing/popular_widgets/drawer.dart';
 import 'package:testing/popular_widgets/appbar_dropdown_button.dart';
@@ -80,6 +81,7 @@ class main_pageState extends State<main_page> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  slideshow(),
                   Padding(
                     padding: EdgeInsets.only(top: 5),
                     child: Text(
@@ -103,9 +105,54 @@ class main_pageState extends State<main_page> {
     );
   }
 }
-/*class profile extends StatelessWidget {
+class slideshow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return ImageSlideshow(
+          /// Width of the [ImageSlideshow].
+          width: double.infinity,
+
+          /// Height of the [ImageSlideshow].
+          height: 200,
+
+          /// The page to show when first creating the [ImageSlideshow].
+          initialPage: 0,
+
+          /// The color to paint the indicator.
+          indicatorColor: Colors.blue,
+
+          /// The color to paint behind th indicator.
+          indicatorBackgroundColor: Colors.grey,
+
+          /// The widgets to display in the [ImageSlideshow].
+          /// Add the sample image file into the images folder
+          children: [
+            Image.asset(
+              'images/sample_image_1.jpg',
+              fit: BoxFit.cover,
+            ),
+            Image.asset(
+              'images/sample_image_2.jpg',
+              fit: BoxFit.cover,
+            ),
+            Image.asset(
+              'images/sample_image_3.jpg',
+              fit: BoxFit.cover,
+            ),
+          ],
+
+          /// Called whenever the page in the center of the viewport changes.
+          onPageChanged: (value) {
+            print('Page changed: $value');
+          },
+
+          /// Auto scroll interval.
+          /// Do not auto scroll with null or 0.
+          autoPlayInterval: 3000,
+
+          /// Loops back to first slide.
+          isLoop: true,
+        ),
 
   }
-  }*/
+  }
