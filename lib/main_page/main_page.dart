07530e88cd5a3a5b5c0eb452a6558da8a4cce31f
FlutLab.io <>
2022-01-11 10:58:03 +0000

    Autosave

--- lib/main_page/main_page.dart
+++ lib/main_page/main_page.dart
@@ -77,7 +77,7 @@ class main_pageState extends State<main_page> {
               child: marquee(),
             ),
             Container(color: theme.splashColor, height: 4),
-            Icon(Icon.exit, color: theme.accentColor),
+            Icon(Icons.exit, color: theme.accentColor),
             slideshow(),
             Container(
               color: theme.backgroundColor,
