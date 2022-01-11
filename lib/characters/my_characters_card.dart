bf16f3b26a42b7e2590d42a8fb25654dc5bd4517
FlutLab.io <>
2022-01-11 02:10:38 +0000

    Autosave

--- lib/characters/my_characters_card.dart
+++ lib/characters/my_characters_card.dart
@@ -14,6 +14,7 @@ class my_characters_card extends StatelessWidget {
       padding: const EdgeInsets.all(8),
       children: <Widget>[
         FlipCard(
+          fill: Fill.fillBack,
           direction: FlipDirection.HORIZONTAL,
           front: Center(
             child: Padding(
