8db84a04cdb5e1b0cf2ba8e245890000d940eb03
FlutLab.io <>
2022-01-11 10:48:23 +0000

    Autosave

--- lib/main_page/slideshow.dart
+++ lib/main_page/slideshow.dart
@@ -16,10 +16,10 @@ class slideshow extends StatelessWidget {
       initialPage: 0,
 
       /// The color to paint the indicator.
-      indicatorColor: theme.accentColor,
+      indicatorColor: theme.backgroundColor,
 
       /// The color to paint behind th indicator.
-      indicatorBackgroundColor: theme.backgroundColor,
+      indicatorBackgroundColor: theme.accentColor,
 
       /// The widgets to display in the [ImageSlideshow].
       /// Add the sample image file into the images folder
