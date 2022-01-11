f9ca2a1f1ef283acb57422db52926df1d25775f7
FlutLab.io <>
2022-01-11 09:23:04 +0000

    Autosave

--- lib/main_page/main_page.dart
+++ lib/main_page/main_page.dart
@@ -105,54 +105,54 @@ class main_pageState extends State<main_page> {
     );
   }
 }
+
 class slideshow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ImageSlideshow(
-          /// Width of the [ImageSlideshow].
-          width: double.infinity,
+      /// Width of the [ImageSlideshow].
+      width: double.infinity,
 
-          /// Height of the [ImageSlideshow].
-          height: 200,
+      /// Height of the [ImageSlideshow].
+      height: 200,
 
-          /// The page to show when first creating the [ImageSlideshow].
-          initialPage: 0,
+      /// The page to show when first creating the [ImageSlideshow].
+      initialPage: 0,
 
-          /// The color to paint the indicator.
-          indicatorColor: Colors.blue,
+      /// The color to paint the indicator.
+      indicatorColor: Colors.blue,
 
-          /// The color to paint behind th indicator.
-          indicatorBackgroundColor: Colors.grey,
+      /// The color to paint behind th indicator.
+      indicatorBackgroundColor: Colors.grey,
 
-          /// The widgets to display in the [ImageSlideshow].
-          /// Add the sample image file into the images folder
-          children: [
-            Image.asset(
-              'images/sample_image_1.jpg',
-              fit: BoxFit.cover,
-            ),
-            Image.asset(
-              'images/sample_image_2.jpg',
-              fit: BoxFit.cover,
-            ),
-            Image.asset(
-              'images/sample_image_3.jpg',
-              fit: BoxFit.cover,
-            ),
-          ],
-
-          /// Called whenever the page in the center of the viewport changes.
-          onPageChanged: (value) {
-            print('Page changed: $value');
-          },
-
-          /// Auto scroll interval.
-          /// Do not auto scroll with null or 0.
-          autoPlayInterval: 3000,
-
-          /// Loops back to first slide.
-          isLoop: true,
+      /// The widgets to display in the [ImageSlideshow].
+      /// Add the sample image file into the images folder
+      children: [
+        Image.asset(
+          'images/sample_image_1.jpg',
+          fit: BoxFit.cover,
         ),
+        Image.asset(
+          'images/sample_image_2.jpg',
+          fit: BoxFit.cover,
+        ),
+        Image.asset(
+          'images/sample_image_3.jpg',
+          fit: BoxFit.cover,
+        ),
+      ],
 
+      /// Called whenever the page in the center of the viewport changes.
+      onPageChanged: (value) {
+        print('Page changed: $value');
+      },
+
+      /// Auto scroll interval.
+      /// Do not auto scroll with null or 0.
+      autoPlayInterval: 3000,
+
+      /// Loops back to first slide.
+      isLoop: true,
+    );
   }
-  }
+}
