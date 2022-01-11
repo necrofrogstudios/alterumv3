d5058f04ea165608b91466bf8f29ef9440d6447f
FlutLab.io <>
2022-01-11 04:59:30 +0000

    Autosave

---
+++ lib/popular_widgets/rules.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import 'package:testing/popular_widgets/drawer.dart';
+import 'package:testing/popular_widgets/appbar_dropdown_button.dart';
+import 'package:testing/popular_widgets/footer.dart';
+import 'package:testing/profile/profile_pics_icons.dart';
+import 'package:testing/profile/profile_tabbar.dart';
+import 'package:testing/popular_widgets/appbar_top_buttons.dart';
+import 'package:testing/popular_widgets/appbar_custom.dart';
+
+class rules extends StatelessWidget {
+  final currentScreen = rules;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(55),
+        child: appbar_custom(),
+      ),
+      drawer: drawer(currentScreen),
+      body: Container(
+        child: ListView(
+          controller: controller,
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(0),
+          children: <Widget>[
+            Container(color: theme.splashColor, height: 4),
+            profile_top_buttons(),
+            profile_pics_icons(),
+            Container(color: theme.splashColor, height: 4),
+            Container(
+              Text(
+                'blep',
+              ),
+            ),
+            Container(color: theme.splashColor, height: 4),
+            footer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*class profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+  }
+  }*/
