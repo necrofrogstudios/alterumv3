e79480058e2de4751f0f90aa858e09bec6eb6433
FlutLab.io <>
2022-01-11 02:14:18 +0000

    Autosave

--- lib/characters/my_characters.dart
+++ lib/characters/my_characters.dart
@@ -49,7 +49,7 @@ class my_characters extends StatelessWidget {
                   children: [
                     roleplay_navigation(),
                     Text(
-                      'MASTERLIST',
+                      'My Characters',
                       style: TextStyle(
                         fontSize: 25,
                         color: theme.primaryColor,
@@ -61,7 +61,7 @@ class my_characters extends StatelessWidget {
                       padding: const EdgeInsets.all(5),
                       crossAxisSpacing: 5,
                       mainAxisSpacing: 5,
-                      crossAxisCount: MediaQuery.of(context).orientation == Orientation.portrait ? 3 : 5,
+                      crossAxisCount: MediaQuery.of(context).orientation == Orientation.portrait ? 1 : 2,
                       children: <Widget>[
                         character_parkjimin(),
                         character_kimjaejoong(),
