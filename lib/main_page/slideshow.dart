7a1f2712decafb17c018e721ff2c76134b69732b
FlutLab.io <>
2022-01-11 10:40:52 +0000

    Autosave

--- lib/main_page/slideshow.dart
+++ lib/main_page/slideshow.dart
@@ -82,7 +82,7 @@ class slideshow extends StatelessWidget {
                 ),
               ),
               child: Padding(
-                padding: EdgeInsets.fromLTRB(3, 1700, 3, 20),
+                padding: EdgeInsets.fromLTRB(3, 170, 3, 20),
                 child: Container(
                   child: FlatButton(
                     color: theme.accentColor,
