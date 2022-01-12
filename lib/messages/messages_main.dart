8490dc50d28815f9f77075ea9996dea3abfc1e0e
FlutLab.io <>
2022-01-12 02:48:09 +0000

    Autosave

--- lib/messages/messages_main.dart
+++ lib/messages/messages_main.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 import 'package:testing/popular_widgets/drawer.dart';
 import 'package:testing/popular_widgets/appbar_dropdown_button.dart';
 import 'package:testing/popular_widgets/footer.dart';
-import 'package:testing/profile/profile_pics_icons.dart';
-import 'package:testing/profile/profile_tabbar.dart';
 import 'package:testing/popular_widgets/appbar_top_buttons.dart';
 import 'package:testing/popular_widgets/appbar_custom.dart';
+import 'package:testing/profile/profile_pics_icons.dart';
+import 'package:testing/profile/profile_tabbar.dart';
 
-class messages_main extends StatelessWidget {
+class messages_main extends StatefulWidget {
+  @override
+  messages_mainState createState() => messages_mainState();
+}
+
+class messages_mainState extends State<messages_main> {
   final currentScreen = messages_main;
+  RefreshController _refreshController = RefreshController(initialRefresh: false);
+
+  List<String> items = [
+    "blep",
+  ];
+
+  void _onRefresh() async {
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
+  }
+
+  void _onLoading() async {
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use loadFailed(),if no data return,use LoadNodata()
+    items.add((items.length + 1).toString());
+    if (mounted) setState(() {});
+    _refreshController.loadComplete();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,15 +49,25 @@ class messages_main extends StatelessWidget {
         child: appbar_custom(),
       ),
       drawer: drawer(currentScreen),
-      body: Container(
+      body: SmartRefresher(
+        enablePullDown: true,
+        header: WaterDropMaterialHeader(backgroundColor: theme.splashColor, color: theme.primaryColor),
+        footer: CustomFooter(
+          builder: (BuildContext context, LoadStatus mode) {},
+        ),
+        controller: _refreshController,
+        onRefresh: _onRefresh,
+        onLoading: _onLoading,
         child: ListView(
+          scrollDirection: Axis.vertical,
           shrinkWrap: true,
           padding: const EdgeInsets.all(0),
           children: <Widget>[
             Container(color: theme.splashColor, height: 4),
             profile_top_buttons(),
+            Container(color: theme.splashColor, height: 4),
             Text(
-              'bleh',
+              'blep',
             ),
             Container(color: theme.splashColor, height: 4),
             footer(),
