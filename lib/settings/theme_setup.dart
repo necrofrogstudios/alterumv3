4461371642afd7dda95d5f79dc392227915de2a9
FlutLab.io <>
2022-01-12 06:11:42 +0000

    Autosave

--- lib/settings/theme_setup.dart
+++ lib/settings/theme_setup.dart
@@ -122,101 +122,121 @@ List<ThemeData> getThemes = [
   ),
   ThemeData(
     //orange, black, white//
-    backgroundColor: Colors.black,
-    canvasColor: Colors.black,
+    backgroundColor: Colors.black, canvasColor: Colors.black,
     accentColor: Colors.orange,
     splashColor: Color(0xFFBDBF00),
     primaryColor: Colors.white,
   ),
   ThemeData(
     //grey, grey, pink, black //
-    backgroundColor: Color(0xFFD6D1CD), canvasColor: Color(0xFFD6D1CD),
+    backgroundColor: Color(0xFFD6D1CD),
+    canvasColor: Color(0xFFD6D1CD),
     accentColor: Color(0xFF969A97),
     splashColor: Color(0xFFC60F7B),
     primaryColor: Colors.black,
   ),
   ThemeData(
     //red, white, black//
-    backgroundColor: Color(0xFFdde5e7), canvasColor: Color(0xFFdde5e7),
+    backgroundColor: Color(0xFFdde5e7),
+    canvasColor: Color(0xFFdde5e7),
     accentColor: Color(0xFFd4674c),
     splashColor: Color(0xFF67727e),
     primaryColor: Colors.black,
   ),
   ThemeData(
     //blue, blue, white, black//
-    backgroundColor: Colors.white, canvasColor: Colors.white,
+    backgroundColor: Colors.white,
+    canvasColor: Colors.white,
     accentColor: Color(0xFF60e3d5),
     splashColor: Color(0xFF0567a8),
     primaryColor: Colors.black,
   ),
   ThemeData(
     //pink, blue, black, black//
-    backgroundColor: Color(0xFFecc19c), canvasColor: Color(0xFFecc19c),
+    backgroundColor: Color(0xFFecc19c),
+    canvasColor: Color(0xFFecc19c),
     accentColor: Color(0xFF1e8475),
     splashColor: Color(0xFF000000),
     primaryColor: Colors.black,
   ),
   ThemeData(
     //blue, pinkx2, white//
-    backgroundColor: Color(0xFF7fe7dc), canvasColor: Color(0xFF7fe7dc),
+    backgroundColor: Color(0xFF7fe7dc),
+    canvasColor: Color(0xFF7fe7dc),
     accentColor: Color(0xFFf47a60),
     splashColor: Color(0xFFf47a60),
     primaryColor: Colors.black,
   ),
   ThemeData(
     //tan, tan, grey, black//
-    backgroundColor: Color(0xFFe5d4cd), canvasColor: Color(0xFFe5d4cd),
+    backgroundColor: Color(0xFFe5d4cd),
+    canvasColor: Color(0xFFe5d4cd),
     accentColor: Color(0xFFb59283),
     splashColor: Color(0xFF304655),
     primaryColor: Colors.black,
   ),
   ThemeData(
     //green, yellow, dark green//
-    backgroundColor: Color(0xFFa8c66c), canvasColor: Color(0xFFa8c66c),
+    backgroundColor: Color(0xFFa8c66c),
+    canvasColor: Color(0xFFa8c66c),
     accentColor: Color(0xFFe1dd72),
     splashColor: Color(0xFF1b6535),
     primaryColor: Colors.black,
   ),
   ThemeData(
     //green, yellow, blue, black//
-    backgroundColor: Color(0xFF41a592), canvasColor: Color(0xFF41a592),
+    backgroundColor: Color(0xFF41a592),
+    canvasColor: Color(0xFF41a592),
     accentColor: Color(0xFFf5ce4b),
     splashColor: Color(0xFF1998d3),
     primaryColor: Colors.black,
   ),
   ThemeData(
     //lavender, lilac, black, black//
-    backgroundColor: Color(0xFF998FC7), canvasColor: Color(0xFF998FC7),
+    backgroundColor: Color(0xFF998FC7),
+    canvasColor: Color(0xFF998FC7),
     accentColor: Color(0xFFD4C2FC),
     splashColor: Color(0xFF28262C),
     primaryColor: Colors.black,
   ),
   ThemeData(
     //green, green, mint, black//
-    backgroundColor: Color(0xFF79B791), canvasColor: Color(0xFF79B791),
+    backgroundColor: Color(0xFF79B791),
+    canvasColor: Color(0xFF79B791),
     accentColor: Color(0xFFABD1B5),
     splashColor: Color(0xFFEDF4ED),
     primaryColor: Colors.black,
   ),
   ThemeData(
     //blue, pink, white, black//
-    backgroundColor: Color(0xFFF9FBF2), canvasColor: Color(0xFFF9FBF2),
+    backgroundColor: Color(0xFFF9FBF2),
+    canvasColor: Color(0xFFF9FBF2),
     accentColor: Color(0xFFFFE7D6),
     splashColor: Color(0xFFADF3FF),
     primaryColor: Colors.black,
   ),
   ThemeData(
     //grey, green, blue, black//
-    backgroundColor: Color(0xFF9DB5B2), canvasColor: Color(0xFF9DB5B2),
+    backgroundColor: Color(0xFF9DB5B2),
+    canvasColor: Color(0xFF9DB5B2),
     accentColor: Color(0xFFD3D5D4),
     splashColor: Color(0xFA2C5AC),
     primaryColor: Colors.black,
   ),
   ThemeData(
     //pink, pink, purple, black//
-    backgroundColor: Color(0xFFB388EB), canvasColor: Color(0xFFB388EB),
+    backgroundColor: Color(0xFFB388EB),
+    canvasColor: Color(0xFFB388EB),
     accentColor: Color(0xFFCC928F),
     splashColor: Color(0xFFC3979C),
+    primaryColor: Colors.black,
+  ),
+  ThemeData(
+    //orange, blue, purple, black//
+    backgroundColor: Color(0xFFE594FF),
+    canvasColor: Color(0xFFE594FF),
+    accentColor: Color(0xFF00DBD9),
+    splashColor: Color(0xFFFFA500),
     primaryColor: Colors.black,
   ),
   ThemeData(
@@ -228,98 +248,112 @@ List<ThemeData> getThemes = [
   ),
   ThemeData(
     //cream, grey, blue, white//
-    backgroundColor: Color(0xFF084C61), canvasColor: Color(0xFF084C61),
+    backgroundColor: Color(0xFF084C61),
+    canvasColor: Color(0xFF084C61),
     accentColor: Color(0xFF177E89),
     splashColor: Color(0xFFF4F1BB),
     primaryColor: Colors.white,
   ),
   ThemeData(
     //oranges, white//
-    backgroundColor: Color(0xFFF56416), canvasColor: Color(0xFFF56416),
+    backgroundColor: Color(0xFFF56416),
+    canvasColor: Color(0xFFF56416),
     accentColor: Color(0xFFDD4B1A),
     splashColor: Color(0xFFE28413),
     primaryColor: Colors.white,
   ),
   ThemeData(
     //blue, blue, green, white//
-    backgroundColor: Color(0xFF005377), canvasColor: Color(0xFF005377),
+    backgroundColor: Color(0xFF005377),
+    canvasColor: Color(0xFF005377),
     accentColor: Color(0xFF052F5F),
     splashColor: Color(0xFF06A77D),
     primaryColor: Colors.white,
   ),
   ThemeData(
     //black, grey, white, white//
-    backgroundColor: Colors.black, canvasColor: Colors.black,
+    backgroundColor: Colors.black,
+    canvasColor: Colors.black,
     accentColor: Color(0xFF394d5f),
     splashColor: Colors.white,
     primaryColor: Colors.white,
   ),
   ThemeData(
     //black, grey, white, white//
-    backgroundColor: Color(0xfffa8e44), canvasColor: Color(0xfffa8e44),
+    backgroundColor: Color(0xfffa8e44),
+    canvasColor: Color(0xfffa8e44),
     accentColor: Color(0xFF96577f),
     splashColor: Color(0xFF1f3095),
     primaryColor: Colors.white,
   ),
   ThemeData(
     //red, black, green, white//
-    backgroundColor: Color(0xFF0F0E0E), canvasColor: Color(0xFF0F0E0E),
+    backgroundColor: Color(0xFF0F0E0E),
+    canvasColor: Color(0xFF0F0E0E),
     accentColor: Color(0xFF541212),
     splashColor: Color(0xFF8B9A46),
     primaryColor: Colors.white,
   ),
   ThemeData(
     //plum, purple, gold, white//
-    backgroundColor: Color(0xFF2E0219), canvasColor: Color(0xFF2E0219),
+    backgroundColor: Color(0xFF2E0219),
+    canvasColor: Color(0xFF2E0219),
     accentColor: Color(0xFF6A0F49),
     splashColor: Color(0xFFb3b300),
     primaryColor: Colors.white,
   ),
   ThemeData(
     //tan, dark brown, teal//
-    backgroundColor: Color(0xFF65532F), canvasColor: Color(0xFF65532F),
+    backgroundColor: Color(0xFF65532F),
+    canvasColor: Color(0xFF65532F),
     accentColor: Color(0xFF312509),
     splashColor: Color(0xFF009b7d),
     primaryColor: Colors.white,
   ),
   ThemeData(
     //plum,purple, pink, white//
-    backgroundColor: Color(0xFF311847), canvasColor: Color(0xFF311847),
+    backgroundColor: Color(0xFF311847),
+    canvasColor: Color(0xFF311847),
     accentColor: Color(0xFFA01A7D),
     splashColor: Color(0xFFEC4067),
     primaryColor: Colors.white,
   ),
   ThemeData(
     //purple, teal, pink, white//
-    backgroundColor: Color(0xFF5c3c92), canvasColor: Color(0xFF5c3c92),
+    backgroundColor: Color(0xFF5c3c92),
+    canvasColor: Color(0xFF5c3c92),
     accentColor: Color(0xFF077b8a),
     splashColor: Color(0xFFd72637),
     primaryColor: Colors.white,
   ),
   ThemeData(
     //black, pink, blue white//
-    backgroundColor: Color(0xFF101b37), canvasColor: Color(0xFF101b37),
+    backgroundColor: Color(0xFF101b37),
+    canvasColor: Color(0xFF101b37),
     accentColor: Color(0xFFe41376),
     splashColor: Color(0xFF01c8ee),
     primaryColor: Colors.white,
   ),
   ThemeData(
     //orange, black, grey, white//
-    backgroundColor: Color(0xFF000000), canvasColor: Color(0xFF000000),
+    backgroundColor: Color(0xFF000000),
+    canvasColor: Color(0xFF000000),
     accentColor: Color(0xFFffa500),
     splashColor: Color(0xFFa9a9a9),
     primaryColor: Colors.white,
   ),
   ThemeData(
     //pink, purple, blue, white//
-    backgroundColor: Color(0xFFba55d3), canvasColor: Color(0xFFba55d3),
+    backgroundColor: Color(0xFFba55d3),
+    canvasColor: Color(0xFFba55d3),
     accentColor: Color(0xFFfe4164),
     splashColor: Color(0xFF0fc0fc),
     primaryColor: Colors.white,
   ),
   ThemeData(
     //spiderman//
-    backgroundColor: Colors.black, canvasColor: Colors.black,
+    backgroundColor: Colors.black,
+    canvasColor: Colors.black,
     accentColor: Color(0xFF050A30),
     splashColor: Color(0xFFFF0000),
     primaryColor: Colors.white,
