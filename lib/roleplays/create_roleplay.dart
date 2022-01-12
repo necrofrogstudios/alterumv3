02b37e9ba9cbccd23326464dce3a05f63b6acfab
FlutLab.io <>
2022-01-12 05:03:01 +0000

    Autosave

--- lib/roleplays/create_roleplay.dart
+++ lib/roleplays/create_roleplay.dart
@@ -317,7 +317,7 @@ class post_button extends StatelessWidget {
               context: context,
               builder: (BuildContext context) => AlertDialog(
                 title: const Text('Head Admin Agreement'),
-                content: const Text('All user generated content must abide by the rules. Upon creating this Roleplay, you are now responsible for reporting any inapporiate behavior such as bullying, harrassment, and inappropriate photos. Failure to do so will cause the roleplay to be terminated with or without warning. Do you accept these terms? '),
+                content: const Text('All user generated content must abide by the rules. Upon creating this Roleplay, you are now responsible for correcting and reporting any inapporiate behavior such as bullying, harrassment, and inappropriate photos. Failure to do so will cause the roleplay to be terminated with or without warning. Do you accept these terms? '),
                 actions: <Widget>[
                   TextButton(
                     onPressed: () => Navigator.pop(context, 'Cancel'),
