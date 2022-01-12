4f5368c84da0e20eaf868bbda7276bbb2117f4f0
FlutLab.io <>
2022-01-12 04:51:50 +0000

    Autosave

--- lib/roleplays/create_roleplay.dart
+++ lib/roleplays/create_roleplay.dart
@@ -313,7 +313,23 @@ class post_button extends StatelessWidget {
               'Create',
               style: TextStyle(fontSize: 15, color: theme.primaryColor),
             ),
-            onPressed: () {},
+            onPressed: () => showDialog<String>(
+              context: context,
+              builder: (BuildContext context) => AlertDialog(
+                title: const Text('AlertDialog Title'),
+                content: const Text('AlertDialog description'),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () => Navigator.pop(context, 'Cancel'),
+                    child: const Text('Cancel'),
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.pop(context, 'OK'),
+                    child: const Text('OK'),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
