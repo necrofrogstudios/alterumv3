2d7786e7d9b991650b497ecc4a68f1797abc1722
FlutLab.io <>
2022-01-12 02:23:01 +0000

    Autosave

--- lib/settings/settings.dart
+++ lib/settings/settings.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:testing/settings/theme_setup.dart';
-import 'package:testing/popular_widgets/drawer.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
-import 'package:testing/settings/themes_model.dart';
 import 'package:stacked_themes/stacked_themes.dart';
 import 'package:stacked/stacked.dart';
+
 import 'package:testing/settings/themes_navigation.dart';
+import 'package:testing/settings/themes_model.dart';
+import 'package:testing/settings/theme_setup.dart';
 import 'package:testing/popular_widgets/appbar_top_buttons.dart';
-
-import 'package:flutter/material.dart';
-import 'package:marquee/marquee.dart';
-import 'package:flutter_icons/flutter_icons.dart';
-
+import 'package:testing/popular_widgets/drawer.dart';
 import 'package:testing/popular_widgets/drawer.dart';
 import 'package:testing/popular_widgets/appbar_dropdown_button.dart';
 import 'package:testing/popular_widgets/appbar_top_buttons.dart';
-import 'package:testing/main_page/marquee.dart';
 import 'package:testing/messages/messages_main.dart';
 import 'package:testing/main_page/main_page.dart';
 
@@ -28,18 +25,39 @@ class settings extends StatefulWidget {
 
 class _settingsState extends State<settings> {
   final currentScreen = settings;
+  RefreshController _refreshController = RefreshController(initialRefresh: false);
   final List<Widget> navigation = [
     main_page(),
     messages_main(),
     main_page(),
     messages_main(),
   ];
+  List<String> items = [
+    "Popular",
+  ];
+
+  void _onRefresh() async {
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
+  }
+
+  void _onLoading() async {
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use loadFailed(),if no data return,use LoadNodata()
+    items.add((items.length + 1).toString());
+    if (mounted) setState(() {});
+    _refreshController.loadComplete();
+  }
 
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
+    Color myColor = Theme.of(context).accentColor;
+
     return Scaffold(
-      backgroundColor: theme.backgroundColor,
       appBar: AppBar(
         centerTitle: true,
         bottomOpacity: 0.0,
@@ -74,19 +92,34 @@ class _settingsState extends State<settings> {
         ],
       ),
       drawer: drawer(currentScreen),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: SingleChildScrollView(
-          physics: ScrollPhysics(),
-          child: Column(
-            children: <Widget>[
-              Container(color: theme.splashColor, height: 4),
-              profile_top_buttons(),
-              Container(color: theme.splashColor, height: 2),
-              themes_navigation(),
-            ],
-          ),
+      body: SmartRefresher(
+        enablePullDown: true,
+        header: WaterDropMaterialHeader(backgroundColor: theme.splashColor, color: theme.primaryColor),
+        footer: CustomFooter(
+          builder: (BuildContext context, LoadStatus mode) {},
+        ),
+        controller: _refreshController,
+        onRefresh: _onRefresh,
+        onLoading: _onLoading,
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(0),
+          children: <Widget>[
+            Container(color: theme.splashColor, height: 4),
+            profile_top_buttons(),
+            Container(color: theme.splashColor, height: 4),
+            Column(
+              children: <Widget>[
+                Container(color: theme.splashColor, height: 4),
+                profile_top_buttons(),
+                Container(color: theme.splashColor, height: 2),
+                themes_navigation(),
+              ],
+            ),
+            Container(color: theme.splashColor, height: 4),
+            footer(),
+          ],
         ),
       ),
     );
