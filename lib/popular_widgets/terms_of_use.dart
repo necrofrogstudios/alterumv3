fd1d6cdf6c59a81fa79e048af17c43a382ea21be
FlutLab.io <>
2022-01-14 09:02:46 +0000

    Autosave

--- lib/popular_widgets/terms_of_use.dart
+++ lib/popular_widgets/terms_of_use.dart
@@ -100,7 +100,7 @@ class terms_of_useState extends State<terms_of_use> {
                         color: theme.primaryColor,
                       ),
                       children: <TextSpan>[
-                        TextSpan(text: 'Hello \n', textAlign: TextAlign.left, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30, color: theme.primaryColor)),
+                        TextSpan(text: 'Hello \n', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30, color: theme.primaryColor)),
                         TextSpan(text: 'World \n', style: TextStyle(fontSize: 15, color: theme.primaryColor)),
                       ],
                     ),
