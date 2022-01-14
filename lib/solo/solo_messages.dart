0cb181f24ee4c55edd745803ae26572d2a3fda96
FlutLab.io <>
2022-01-14 05:24:28 +0000

    Autosave

--- lib/solo/solo_messages.dart
+++ lib/solo/solo_messages.dart
@@ -1,9 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:swiping_card_deck/swiping_card_deck.dart';
 import 'dart:math' as math;
+import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
-class ExamplePage extends StatelessWidget {
-  const ExamplePage();
+import 'package:testing/popular_widgets/drawer.dart';
+import 'package:testing/popular_widgets/appbar_dropdown_button.dart';
+import 'package:testing/popular_widgets/footer.dart';
+import 'package:testing/popular_widgets/appbar_top_buttons.dart';
+import 'package:testing/popular_widgets/appbar_custom.dart';
+import 'package:testing/profile/profile_pics_icons.dart';
+import 'package:testing/profile/profile_tabbar.dart';
+
+class messages_main extends StatefulWidget {
+  @override
+  messages_mainState createState() => messages_mainState();
+}
+
+class messages_mainState extends State<messages_main> {
+  final currentScreen = messages_main;
+  RefreshController _refreshController = RefreshController(initialRefresh: false);
+
+  List<String> items = [
+    "blep",
+  ];
+
+  void _onRefresh() async {
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
+  }
+
+  void _onLoading() async {
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use loadFailed(),if no data return,use LoadNodata()
+    items.add((items.length + 1).toString());
+    if (mounted) setState(() {});
+    _refreshController.loadComplete();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,31 +52,63 @@ class ExamplePage extends StatelessWidget {
       swipeThreshold: MediaQuery.of(context).size.width / 3,
       minimumVelocity: 1000,
     );
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        deck,
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            IconButton(
-              icon: const Icon(Icons.clear),
-              iconSize: 30,
-              color: Colors.red,
-              onPressed: deck.animationActive ? null : () => deck.swipeLeft(),
+    var theme = Theme.of(context);
+    Color myColor = Theme.of(context).accentColor;
+
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(55),
+        child: appbar_custom(),
+      ),
+      drawer: drawer(currentScreen),
+      body: SmartRefresher(
+        enablePullDown: true,
+        header: WaterDropMaterialHeader(backgroundColor: theme.splashColor, color: theme.primaryColor),
+        footer: CustomFooter(
+          builder: (BuildContext context, LoadStatus mode) {},
+        ),
+        controller: _refreshController,
+        onRefresh: _onRefresh,
+        onLoading: _onLoading,
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(0),
+          children: <Widget>[
+            Container(color: theme.splashColor, height: 4),
+            profile_top_buttons(),
+            Container(color: theme.splashColor, height: 4),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                deck,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.clear),
+                      iconSize: 30,
+                      color: Colors.red,
+                      onPressed: deck.animationActive ? null : () => deck.swipeLeft(),
+                    ),
+                    const SizedBox(width: 40),
+                    IconButton(
+                      icon: const Icon(Icons.check),
+                      iconSize: 30,
+                      color: Colors.green,
+                      onPressed: deck.animationActive ? null : () => deck.swipeRight(),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            const SizedBox(width: 40),
-            IconButton(
-              icon: const Icon(Icons.check),
-              iconSize: 30,
-              color: Colors.green,
-              onPressed: deck.animationActive ? null : () => deck.swipeRight(),
-            ),
+            Container(color: theme.splashColor, height: 4),
+            footer(),
           ],
         ),
-      ],
+      ),
     );
   }
 
@@ -54,3 +122,10 @@ class ExamplePage extends StatelessWidget {
     return cardDeck;
   }
 }
+
+/*class profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+  }
+  }*/
