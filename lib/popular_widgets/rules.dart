1d85c47184348f2e6d62c1f5c1f689032b6d6a7b
FlutLab.io <>
2022-01-14 06:54:18 +0000

    Autosave

--- lib/popular_widgets/rules.dart
+++ lib/popular_widgets/rules.dart
@@ -11,9 +11,32 @@ import 'package:testing/popular_widgets/appbar_custom.dart';
 class rules extends StatelessWidget {
   final currentScreen = rules;
 
+  RefreshController _refreshController = RefreshController(initialRefresh: false);
+
+  List<String> items = [
+    "blep",
+  ];
+
+  void _onRefresh() async {
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
+  }
+
+  void _onLoading() async {
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use loadFailed(),if no data return,use LoadNodata()
+    items.add((items.length + 1).toString());
+    if (mounted) setState(() {});
+    _refreshController.loadComplete();
+  }
+
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
+    Color myColor = Theme.of(context).accentColor;
 
     return Scaffold(
       appBar: PreferredSize(
@@ -21,18 +44,25 @@ class rules extends StatelessWidget {
         child: appbar_custom(),
       ),
       drawer: drawer(currentScreen),
-      body: Container(
+      body: SmartRefresher(
+        enablePullDown: true,
+        header: WaterDropMaterialHeader(backgroundColor: theme.splashColor, color: theme.primaryColor),
+        footer: CustomFooter(
+          builder: (BuildContext context, LoadStatus mode) {},
+        ),
+        controller: _refreshController,
+        onRefresh: _onRefresh,
+        onLoading: _onLoading,
         child: ListView(
+          scrollDirection: Axis.vertical,
           shrinkWrap: true,
           padding: const EdgeInsets.all(0),
           children: <Widget>[
             Container(color: theme.splashColor, height: 4),
             profile_top_buttons(),
             Container(color: theme.splashColor, height: 4),
-            Container(
-              child: Text(
-                'blep',
-              ),
+            Text(
+              'blep',
             ),
             Container(color: theme.splashColor, height: 4),
             footer(),
