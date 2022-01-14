8101a303b6a3de79ef6ad791f3d1b842c5af1c99
FlutLab.io <>
2022-01-14 09:21:39 +0000

    Autosave

--- lib/popular_widgets/terms_of_use.dart
+++ lib/popular_widgets/terms_of_use.dart
@@ -97,7 +97,7 @@ class terms_of_useState extends State<terms_of_use> {
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.fromLTRB(10, 5, 10, 0),
+                  padding: EdgeInsets.fromLTRB(10, 5, 10, 10),
                   child: Text(
                     'These terms and conditions (“Agreement”) set forth the general terms and conditions of your use of the “Alterum” mobile application (“Mobile Application” or “Service”) and any of its related products and services (collectively, “Services”). This Agreement is legally binding between you (“User”, “you” or “your”) and this Mobile Application developer (“Operator”, “we”, “us” or “our”). If you are entering into this agreement on behalf of a business or other legal entity, you represent that you have the authority to bind such entity to this agreement, in which case the terms “User”, “you” or “your” shall refer to such entity. If you do not have such authority, or if you do not agree with the terms of this agreement, you must not accept this agreement and may not access and use the Mobile Application and Services. By accessing and using the Mobile Application and Services, you acknowledge that you have read, understood, and agree to be bound by the terms of this Agreement. You acknowledge that this Agreement is a contract between you and the Operator, even though it is electronic and is not physically signed by you, and it governs your use of the Mobile Application and Services.',
                     style: TextStyle(
@@ -114,7 +114,7 @@ class terms_of_useState extends State<terms_of_use> {
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.fromLTRB(10, 5, 10, 0),
+                  padding: EdgeInsets.fromLTRB(10, 5, 10, 10),
                   child: Text(
                     'You must be at least 13 years of age to use the Mobile Application and Services. By using the Mobile Application and Services and by agreeing to this Agreement you warrant and represent that you are at least 13 years of age. If you create an account in the Mobile Application, you are responsible for maintaining the security of your account and you are fully responsible for all activities that occur under the account and any other actions taken in connection with it. We may monitor and review new accounts before you may sign in and start using the Services. Providing false contact information of any kind may result in the termination of your account. You must immediately notify us of any unauthorized uses of your account or any other breaches of security. We will not be liable for any acts or omissions by you, including any damages of any kind incurred as a result of such acts or omissions. We may suspend, disable, or delete your account (or any part thereof) if we determine that you have violated any provision of this Agreement or that your conduct or content would tend to damage our reputation and goodwill. If we delete your account for the foregoing reasons, you may not re-register for our Services. We may block your email address and Internet protocol address to prevent further registration.',
                     style: TextStyle(
@@ -131,7 +131,7 @@ class terms_of_useState extends State<terms_of_use> {
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.fromLTRB(10, 5, 10, 0),
+                  padding: EdgeInsets.fromLTRB(10, 5, 10, 10),
                   child: Text(
                     'We do not own any data, information or material (collectively, “Content”) that you submit in the Mobile Application in the course of using the Service. You shall have sole responsibility for the accuracy, quality, integrity, legality, reliability, appropriateness, and intellectual property ownership or right to use of all submitted Content. We may monitor and review the Content in the Mobile Application submitted or created using our Services by you. You grant us permission to access, copy, distribute, store, transmit, reformat, display and perform the Content of your user account solely as required for the purpose of providing the Services to you. Without limiting any of those representations or warranties, we have the right, though not the obligation, to, in our own sole discretion, refuse or remove any Content that, in our reasonable opinion, violates any of our policies or is in any way harmful or objectionable. You also grant us the license to use, reproduce, adapt, modify, publish or distribute the Content created by you or stored in your user account for commercial, marketing or any similar purpose.',
                     style: TextStyle(
@@ -148,7 +148,7 @@ class terms_of_useState extends State<terms_of_use> {
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.fromLTRB(10, 5, 10, 0),
+                  padding: EdgeInsets.fromLTRB(10, 5, 10, 10),
                   child: Text(
                     'v',
                     style: TextStyle(
@@ -165,7 +165,7 @@ class terms_of_useState extends State<terms_of_use> {
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.fromLTRB(10, 5, 10, 0),
+                  padding: EdgeInsets.fromLTRB(10, 5, 10, 10),
                   child: Text(
                     'v',
                     style: TextStyle(
@@ -182,7 +182,7 @@ class terms_of_useState extends State<terms_of_use> {
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.fromLTRB(10, 5, 10, 0),
+                  padding: EdgeInsets.fromLTRB(10, 5, 10, 10),
                   child: Text(
                     'v',
                     style: TextStyle(
@@ -199,7 +199,7 @@ class terms_of_useState extends State<terms_of_use> {
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.fromLTRB(10, 5, 10, 0),
+                  padding: EdgeInsets.fromLTRB(10, 5, 10, 10),
                   child: Text(
                     'v',
                     style: TextStyle(
