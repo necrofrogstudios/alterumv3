4e7fab4e882c5d5577afec929ed20a0367e5560e
FlutLab.io <>
2022-01-18 03:15:15 +0000

    Autosave

--- lib/sign_in/sign_up.dart
+++ lib/sign_in/sign_up.dart
@@ -20,7 +20,9 @@ class sign_upState extends State<sign_up> {
         constraints: BoxConstraints.expand(),
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: NetworkImage("https://64.media.tumblr.com/9b7ac6e7d24cf994c538d166ff330f53/e05d144f651b2db4-a6/s400x600/50ed90a10be694dfd4203ec45627f5c1f355c69e.jpg"),
+            image: Image(
+              image: AssetImage('images/maskspic.png'),
+            ),
             fit: BoxFit.cover,
           ),
         ),
