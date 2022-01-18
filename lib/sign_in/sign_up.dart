b4cfb1ca0c0a95667f1b4dc60f4b06910e6e73b3
FlutLab.io <>
2022-01-18 06:40:46 +0000

    Autosave

--- lib/sign_in/sign_up.dart
+++ lib/sign_in/sign_up.dart
@@ -375,11 +375,8 @@ class register_button extends StatelessWidget {
                   color: Colors.black,
                 ),
                 children: <TextSpan>[
-                  TextSpan(text: 'By creating an account, you agree to Alterum\s' ),
-                  TextSpan(text: 'Terms of Service',recognizer: TapGestureRecognizer()
-                onTap = () {
-                  print('Privacy Policy"'),
-                }, style: const TextStyle(fontWeight: FontWeight.bold)),
+                  TextSpan(text: 'By creating an account, you agree to Alterum\s ),
+                  TextSpan(text: 'Terms of Service', style: const TextStyle(fontWeight: FontWeight.bold)),
                 ],
               ),
             ),
