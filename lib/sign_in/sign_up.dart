fbc7e0975030ee2ecb6677840b4026485a96a578
FlutLab.io <>
2022-01-18 06:22:36 +0000

    Autosave

--- lib/sign_in/sign_up.dart
+++ lib/sign_in/sign_up.dart
@@ -35,12 +35,46 @@ class sign_upState extends State<sign_up> {
                     padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
                     child: IconButton(
                       icon: const Icon(Icons.arrow_back, color: Colors.white, size: 30),
-                      onPressed: () {
-                        Navigator.pop(
-                          context,
-                          MaterialPageRoute(builder: (context) => sign_in()),
-                        );
-                      },
+                      onPressed: () => showDialog<String>(
+                        context: context,
+                        builder: (BuildContext context) => AlertDialog(
+                          backgroundColor: theme.accentColor,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                            Radius.circular(30),
+                          )),
+                          title: Text(
+                            'Head Admin Agreement',
+                            style: TextStyle(color: theme.primaryColor, fontSize: 15),
+                          ),
+                          content: Text(
+                            'All user generated content must abide by the rules. Upon creating this Roleplay, you are now responsible for correcting and reporting any inapporiate behavior such as bullying, harrassment, and inappropriate photos. Failure to do so will cause the roleplay to be terminated with or without warning. Do you accept these terms? ',
+                            style: TextStyle(color: theme.primaryColor, fontSize: 15),
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () => Navigator.pop(
+                                context,
+                                'Cancel',
+                              ),
+                              child: Text(
+                                'No',
+                                style: TextStyle(color: theme.primaryColor, fontSize: 15),
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => profile()),
+                              ),
+                              child: Text(
+                                'Yes',
+                                style: TextStyle(color: theme.primaryColor, fontSize: 15),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                 ],
