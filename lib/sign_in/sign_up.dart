7dbe66fbc72e0e218e0d62de6bac575504da3af7
FlutLab.io <>
2022-01-18 03:19:14 +0000

    Autosave

--- lib/sign_in/sign_up.dart
+++ lib/sign_in/sign_up.dart
@@ -47,7 +47,7 @@ class sign_upState extends State<sign_up> {
               ),
               Container(
                 height: 120,
-                child: Image.network("https://icons.iconarchive.com/icons/icons8/windows-8/512/Cinema-Theatre-Masks-icon.png"),
+                child: AssetImage('images/masks.png'),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
