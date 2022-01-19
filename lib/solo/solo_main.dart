1bb0292051eb9d8a26acac0d03047e42dc51ad61
FlutLab.io <>
2022-01-19 11:23:26 +0000

    Autosave

--- lib/solo/solo_main.dart
+++ lib/solo/solo_main.dart
@@ -145,6 +145,7 @@ class solo_mainState extends State<solo_main> with TickerProviderStateMixin {
                                   ),
                                 ),
                                 height: 400,
+                                width: 400,
                                 child: Column(
                                   children: <Widget>[],
                                 ),
