e5f752794fb76aa6b9063ebf1672b2040a71ad39
FlutLab.io <>
2022-01-19 04:59:32 +0000

    Autosave

--- lib/solo/solo_messages.dart
+++ lib/solo/solo_messages.dart
@@ -134,12 +134,12 @@ class ExamplePageState extends State<ExamplePage> {
                 ),
                 child: Column(
                   children: <Widget>[
-                    ClipRRect(
+                    /* ClipRRect(
                       borderRadius: BorderRadius.circular(29.0),
                       child: _swipeItems[index].content.child,
-                    ),
+                    ),*/
                     Text(
-                      _swipeItems[index].content.text,
+                      'yep',
                       style: TextStyle(fontSize: 20, color: theme.primaryColor),
                     ),
                   ],
@@ -161,7 +161,7 @@ class ExamplePageState extends State<ExamplePage> {
                     Padding(
                       padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
                       child: Text(
-                        _swipeItems[index].content.text2,
+                        '_swipeItems[index].content.text2,',
                         style: TextStyle(fontSize: 20, color: theme.primaryColor),
                       ),
                     ),
