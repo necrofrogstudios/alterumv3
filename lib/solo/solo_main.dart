1f46db55d0297dbc7bf5b5db7f67964797fe7422
FlutLab.io <>
2022-01-19 06:31:12 +0000

    Autosave

--- lib/solo/solo_main.dart
+++ lib/solo/solo_main.dart
@@ -132,7 +132,89 @@ class solo_mainState extends State<solo_main> with TickerProviderStateMixin {
                       minWidth: MediaQuery.of(context).size.width * 0.8,
                       minHeight: MediaQuery.of(context).size.width * 0.8,
                       cardBuilder: (context, index) => Card(
-                        child: Image.network('${welcomeImages[index]}'),
+                        child: FlipCard(
+                          fill: Fill.fillBack,
+                          direction: FlipDirection.HORIZONTAL,
+                          front: Center(
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(0, 90, 0, 0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30),
+                                  border: Border.all(
+                                    color: theme.splashColor,
+                                    width: 4,
+                                  ),
+                                ),
+                                height: 200,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(26.0),
+                                  child: Image.network('https://simg.nicepng.com/png/small/8-87422_alien-comments-alien-avatar-red.png'),
+                                ),
+                              ),
+                            ),
+                          ), //back of card  V V V V //
+                          back: Center(
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(0, 90, 0, 0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: theme.accentColor,
+                                  borderRadius: BorderRadius.circular(26),
+                                  border: Border.all(
+                                    color: theme.splashColor,
+                                    width: 4,
+                                  ),
+                                ),
+                                height: 200,
+                                width: 200,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Icon(
+                                          Icons.person_add,
+                                          size: 40,
+                                          color: theme.primaryColor,
+                                        ),
+                                        IconButton(
+                                          icon: Icon(
+                                            Icons.email,
+                                          ),
+                                          iconSize: 40,
+                                          color: theme.primaryColor,
+                                          onPressed: () {
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(builder: (context) => messages_main()),
+                                            );
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Icon(
+                                          Icons.group_add,
+                                          size: 47,
+                                          color: theme.primaryColor,
+                                        ),
+                                        Icon(
+                                          Icons.block,
+                                          size: 40,
+                                          color: theme.primaryColor,
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
                       cardController: controller = CardController(),
                       swipeUpdateCallback: (DragUpdateDetails details, Alignment align) {
