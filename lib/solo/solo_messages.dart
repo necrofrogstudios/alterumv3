a3c33acf3b8ee6c9fdc8438fcc16e20b47450890
FlutLab.io <>
2022-01-19 05:31:38 +0000

    Autosave

--- lib/solo/solo_messages.dart
+++ lib/solo/solo_messages.dart
@@ -3,7 +3,7 @@ import 'package:swiping_card_deck/swiping_card_deck.dart';
 import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
-import 'package:flip_card/flip_card.dart';
+import 'package:flutter_tindercard/flutter_tindercard.dart';
 
 import 'package:testing/popular_widgets/drawer.dart';
 import 'package:testing/popular_widgets/appbar_dropdown_button.dart';
@@ -18,7 +18,15 @@ class ExamplePage extends StatefulWidget {
   ExamplePageState createState() => ExamplePageState();
 }
 
-class ExamplePageState extends State<ExamplePage> {
+class ExamplePageState extends State<ExamplePage> with TickerProviderStateMixin {
+  List<String> welcomeImages = [
+    "assets/welcome0.png",
+    "assets/welcome1.png",
+    "assets/welcome2.png",
+    "assets/welcome2.png",
+    "assets/welcome1.png",
+    "assets/welcome1.png"
+  ];
   final currentScreen = ExamplePage;
   RefreshController _refreshController = RefreshController(initialRefresh: false);
 
@@ -78,31 +86,35 @@ class ExamplePageState extends State<ExamplePage> {
             Container(color: theme.splashColor, height: 4),
             profile_top_buttons(),
             Container(color: theme.splashColor, height: 4),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                deck,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.clear),
-                      iconSize: 30,
-                      color: Colors.red,
-                      onPressed: deck.animationActive ? null : () => deck.swipeLeft(),
-                    ),
-                    const SizedBox(width: 40),
-                    IconButton(
-                      icon: const Icon(Icons.check),
-                      iconSize: 30,
-                      color: Colors.green,
-                      onPressed: deck.animationActive ? null : () => deck.swipeRight(),
-                    ),
-                  ],
+            Container(
+              height: MediaQuery.of(context).size.height * 0.6,
+              child: new TinderSwapCard(
+                swipeUp: true,
+                swipeDown: true,
+                orientation: AmassOrientation.BOTTOM,
+                totalNum: welcomeImages.length,
+                stackNum: 3,
+                swipeEdge: 4.0,
+                maxWidth: MediaQuery.of(context).size.width * 0.9,
+                maxHeight: MediaQuery.of(context).size.width * 0.9,
+                minWidth: MediaQuery.of(context).size.width * 0.8,
+                minHeight: MediaQuery.of(context).size.width * 0.8,
+                cardBuilder: (context, index) => Card(
+                  child: Image.asset('${welcomeImages[index]}'),
                 ),
-              ],
+                cardController: controller = CardController(),
+                swipeUpdateCallback: (DragUpdateDetails details, Alignment align) {
+                  /// Get swiping card's alignment
+                  if (align.x < 0) {
+                    //Card is LEFT swiping
+                  } else if (align.x > 0) {
+                    //Card is RIGHT swiping
+                  }
+                },
+                swipeCompleteCallback: (CardSwipeOrientation orientation, int index) {
+                  /// Get orientation & index of swiped card!
+                },
+              ),
             ),
             Container(color: theme.splashColor, height: 4),
             footer(),
@@ -110,68 +122,6 @@ class ExamplePageState extends State<ExamplePage> {
         ),
       ),
     );
-  }
-
-  List<Card> getCardDeck() {
-    var theme = Theme.of(context);
-    List<Card> cardDeck = [];
-    for (int i = 0; i < 500; ++i) {
-      cardDeck.add(
-        Card(
-          color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0),
-          child: FlipCard(
-            fill: Fill.fillBack,
-            direction: FlipDirection.HORIZONTAL,
-            front: Padding(
-              padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
-              child: Container(
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: theme.accentColor,
-                  border: Border.all(color: theme.splashColor, width: 3.0),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(30),
-                  ),
-                ),
-                child: Column(
-                  children: <Widget>[
-                    /* ClipRRect(
-                      borderRadius: BorderRadius.circular(29.0),
-                      child: _swipeItems[index].content.child,
-                    ),*/
-                    Text(
-                      'yep \n'
-                      'yep \n'
-                      'yep \n'
-                      'yep \n'
-                      'yep \n'
-                      'yep \n'
-                      'yep \n'
-                      'yep \n'
-                      'yep \n',
-                      style: TextStyle(fontSize: 20, color: theme.primaryColor),
-                    ),
-                  ],
-                ),
-              ),
-            ), // BACK OF CARD //
-            back: Padding(
-              padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: theme.accentColor,
-                  border: Border.all(color: theme.splashColor, width: 3.0),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(30),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      );
-    }
-    return cardDeck;
   }
 }
 
