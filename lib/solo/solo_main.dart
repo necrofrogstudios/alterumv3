d7def92f8c4fc76e7bca7b091746e23e7b0cd528
FlutLab.io <>
2022-01-19 06:02:40 +0000

    Autosave

--- lib/solo/solo_main.dart
+++ lib/solo/solo_main.dart
@@ -199,68 +199,6 @@ class solo_mainState extends State<solo_main> {
                       },
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        OutlinedButton(
-                          style: OutlinedButton.styleFrom(
-                            side: BorderSide(width: 3.0, color: theme.accentColor),
-                            elevation: 6,
-                            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                            backgroundColor: theme.backgroundColor,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                          ),
-                          onPressed: () {
-                            _matchEngine.currentItem?.nope();
-                          },
-                          child: Text(
-                            "Nope",
-                            style: TextStyle(color: theme.primaryColor, fontSize: 15),
-                          ),
-                        ),
-                        OutlinedButton(
-                          style: OutlinedButton.styleFrom(
-                            side: BorderSide(width: 3.0, color: theme.accentColor),
-                            elevation: 6,
-                            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                            backgroundColor: theme.backgroundColor,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                          ),
-                          onPressed: () {
-                            _matchEngine.currentItem?.superLike();
-                          },
-                          child: Text(
-                            "Super Like",
-                            style: TextStyle(color: theme.primaryColor, fontSize: 15),
-                          ),
-                        ),
-                        OutlinedButton(
-                          style: OutlinedButton.styleFrom(
-                            side: BorderSide(width: 3.0, color: theme.accentColor),
-                            elevation: 6,
-                            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                            backgroundColor: theme.backgroundColor,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                          ),
-                          onPressed: () {
-                            _matchEngine.currentItem?.like();
-                          },
-                          child: Text(
-                            "Like",
-                            style: TextStyle(color: theme.primaryColor, fontSize: 15),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
                 ],
               ),
             ),
@@ -269,43 +207,5 @@ class solo_mainState extends State<solo_main> {
         ),
       ),
     );
-  }
-
-  @override
-  void initState() {
-    for (int i = 0; i < names.length; i++) {
-      _swipeItems.add(
-        SwipeItem(
-          content: Content(text: names[i], text2: info[i], child: images[i]),
-          likeAction: () {
-            _scaffoldKey.currentState.showSnackBar(
-              SnackBar(
-                content: Text("Liked ${names[i]}"),
-                duration: Duration(milliseconds: 500),
-              ),
-            );
-          },
-          nopeAction: () {
-            _scaffoldKey.currentState.showSnackBar(
-              SnackBar(
-                content: Text("Nope ${names[i]}"),
-                duration: Duration(milliseconds: 500),
-              ),
-            );
-          },
-          superlikeAction: () {
-            _scaffoldKey.currentState.showSnackBar(
-              SnackBar(
-                content: Text("Superliked ${names[i]}"),
-                duration: Duration(milliseconds: 500),
-              ),
-            );
-          },
-        ),
-      );
-    }
-    _swipeItems.shuffle();
-    _matchEngine = MatchEngine(swipeItems: _swipeItems);
-    super.initState();
   }
 }
