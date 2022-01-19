45e8905881bf11eb34ec7de1e9e4732462a62aad
FlutLab.io <>
2022-01-19 08:20:31 +0000

    Autosave

--- lib/solo/solo_main.dart
+++ lib/solo/solo_main.dart
@@ -135,7 +135,7 @@ class solo_mainState extends State<solo_main> with TickerProviderStateMixin {
                               child: Container(
                                 decoration: BoxDecoration(
                                   image: DecorationImage(
-                                    image: networkImage('https://th.bing.com/th/id/OIP._qHh64G9n7v_I4ThR3dEngHaHa?pid=ImgDet&rs=1'),
+                                    image: NetworkImage('${images[index]}'),
                                     fit: BoxFit.fill,
                                   ),
                                   color: theme.accentColor,
