86491fc76afaa495f96c9be93928e5150002afae
FlutLab.io <>
2022-01-20 05:26:22 +0000

    Autosave

--- lib/solo/solo_messages.dart
+++ lib/solo/solo_messages.dart
@@ -108,7 +108,7 @@ class ExamplePageState extends State<ExamplePage> with TickerProviderStateMixin 
                 },
               ),
             ),
-            Container(color: theme.splashColor, height: 4, child: Text('${items[1]}')),
+            Container(color: theme.splashColor, height: 4, child: Text('${items[index]}')),
             footer(),
           ],
         ),
