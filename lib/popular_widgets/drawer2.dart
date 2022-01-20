70d66eca7009e4b3cbc70b01193dec61cdb6994a
FlutLab.io <>
2022-01-20 07:16:53 +0000

    Autosave

--- lib/popular_widgets/drawer2.dart
+++ lib/popular_widgets/drawer2.dart
@@ -139,7 +139,7 @@ class drawer2 extends StatelessWidget {
                   Column(
                     children: [
                       Text(
-                        'basketcase101fasdkfasdfjasdlkfasdf',
+                        'basketcase101fasdk',
                         style: TextStyle(color: theme.primaryColor, fontSize: 15),
                       ),
                     ],
