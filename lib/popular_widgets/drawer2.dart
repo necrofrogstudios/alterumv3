33d48ac998139eea878a9546fdcb52a20bc2cec9
FlutLab.io <>
2022-01-20 09:17:48 +0000

    Autosave

--- lib/popular_widgets/drawer2.dart
+++ lib/popular_widgets/drawer2.dart
@@ -157,7 +157,7 @@ class drawer2 extends StatelessWidget {
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
                                 Padding(
-                                  padding: EdgeInsets.fromLTRB(20, 22, 0, 0),
+                                  padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
                                   child: Icon(
                                     Icons.arrow_back,
                                     color: theme.primaryColor,
