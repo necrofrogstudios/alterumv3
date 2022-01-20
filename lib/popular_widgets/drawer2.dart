2a831cf001fe88cb0dc276289b65132e07421c20
FlutLab.io <>
2022-01-20 09:24:55 +0000

    Autosave

--- lib/popular_widgets/drawer2.dart
+++ lib/popular_widgets/drawer2.dart
@@ -164,12 +164,13 @@ class drawer2 extends StatelessWidget {
                                   ),
                                 ),
                                 Padding(
-                            padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
-                            child:Text(
-                                  'Back',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: theme.primaryColor,
+                                  padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                  child: Text(
+                                    'Back',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: theme.primaryColor,
+                                    ),
                                   ),
                                 ),
                               ],
