de040fea2b6693a014b1e28b7c453513470a2075
FlutLab.io <>
2022-01-20 07:09:50 +0000

    Autosave

--- lib/popular_widgets/drawer2.dart
+++ lib/popular_widgets/drawer2.dart
@@ -130,6 +130,11 @@ class drawer2 extends StatelessWidget {
               ),
             ),
             Container(color: theme.splashColor, height: 4),
+            Row(
+              children: [
+                drawer_flip_card(),
+              ],
+            ),
             drawer_flip_card(),
             Container(
               color: theme.backgroundColor,
@@ -216,94 +221,87 @@ class drawer_flip_card extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    return ListView(
-      physics: NeverScrollableScrollPhysics(),
-      shrinkWrap: true,
-      padding: const EdgeInsets.all(0),
-      children: <Widget>[
-        FlipCard(
-          direction: FlipDirection.HORIZONTAL,
-          front: Center(
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  border: Border.all(
-                    color: theme.splashColor,
-                    width: 4,
-                  ),
-                ),
-                height: 150,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(27.0),
-                  child: Image.network('https://simg.nicepng.com/png/small/8-87422_alien-comments-alien-avatar-red.png'),
-                ),
+    return FlipCard(
+      direction: FlipDirection.HORIZONTAL,
+      front: Center(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              border: Border.all(
+                color: theme.splashColor,
+                width: 4,
               ),
             ),
-          ), //back of card  V V V V //
-          back: Center(
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: theme.accentColor,
-                  borderRadius: BorderRadius.circular(26),
-                  border: Border.all(
-                    color: theme.splashColor,
-                    width: 4,
-                  ),
-                ),
-                height: 150,
-                width: 150,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Icon(
-                          Icons.person_add,
-                          size: 40,
-                          color: theme.primaryColor,
-                        ),
-                        IconButton(
-                          icon: Icon(
-                            Icons.email,
-                          ),
-                          iconSize: 40,
-                          color: theme.primaryColor,
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => messages_main()),
-                            );
-                          },
-                        ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Icon(
-                          Icons.group_add,
-                          size: 47,
-                          color: theme.primaryColor,
-                        ),
-                        Icon(
-                          Icons.block,
-                          size: 40,
-                          color: theme.primaryColor,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
+            height: 150,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(27.0),
+              child: Image.network('https://simg.nicepng.com/png/small/8-87422_alien-comments-alien-avatar-red.png'),
             ),
           ),
         ),
-      ],
+      ), //back of card  V V V V //
+      back: Center(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+          child: Container(
+            decoration: BoxDecoration(
+              color: theme.accentColor,
+              borderRadius: BorderRadius.circular(26),
+              border: Border.all(
+                color: theme.splashColor,
+                width: 4,
+              ),
+            ),
+            height: 150,
+            width: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(
+                      Icons.person_add,
+                      size: 40,
+                      color: theme.primaryColor,
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.email,
+                      ),
+                      iconSize: 40,
+                      color: theme.primaryColor,
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => messages_main()),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(
+                      Icons.group_add,
+                      size: 47,
+                      color: theme.primaryColor,
+                    ),
+                    Icon(
+                      Icons.block,
+                      size: 40,
+                      color: theme.primaryColor,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
