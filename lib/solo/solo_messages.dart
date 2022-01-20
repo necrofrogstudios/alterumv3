9fe282cd5d50bb2802a190f0ea9069e887a0ec26
FlutLab.io <>
2022-01-20 05:22:14 +0000

    Autosave

--- lib/solo/solo_messages.dart
+++ lib/solo/solo_messages.dart
@@ -91,7 +91,7 @@ class ExamplePageState extends State<ExamplePage> with TickerProviderStateMixin 
                 maxHeight: MediaQuery.of(context).size.width * 0.9,
                 minWidth: MediaQuery.of(context).size.width * 0.8,
                 minHeight: MediaQuery.of(context).size.width * 0.8,
-                cardBuilder: (context, index) => Card(
+                cardBuilder: Card(
                   child: Image.asset('${welcomeImages[index]}'),
                 ),
                 cardController: controller = CardController(),
@@ -102,6 +102,9 @@ class ExamplePageState extends State<ExamplePage> with TickerProviderStateMixin 
                   } else if (align.x > 0) {
                     //Card is RIGHT swiping
                   }
+                },
+                swipeCompleteCallback: (CardSwipeOrientation orientation, int index) {
+                  /// Get orientation & index of swiped card!
                 },
               ),
             ),
