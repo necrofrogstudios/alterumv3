9bab44e288b77b3caaed665c6280ae8ad67d6c47
FlutLab.io <>
2022-01-21 01:50:32 +0000

    Autosave

--- lib/popular_widgets/drawer.dart
+++ lib/popular_widgets/drawer.dart
@@ -98,8 +98,8 @@ class drawer extends StatelessWidget {
     }
     return ClipRRect(
       borderRadius: BorderRadius.only(
-        topRight: Radius.circular(360),
-        bottomRight: Radius.circular(360),
+        topRight: Radius.circular(380),
+        bottomRight: Radius.circular(380),
       ),
       child: Drawer(
         child: Container(
