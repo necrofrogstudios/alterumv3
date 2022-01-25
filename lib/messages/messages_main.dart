5689f7e4589a78c8fcd0abd90e538f5ee5f2cfc1
FlutLab.io <>
2022-01-25 06:56:43 +0000

    Autosave

--- lib/messages/messages_main.dart
+++ lib/messages/messages_main.dart
@@ -136,21 +136,6 @@ class messages_mainState extends State<messages_main> {
                                 ),
                               ],
                             ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(20, 5, 10, 20),
-                              child: Column(
-                                children: <Widget>[
-                                  Align(
-                                    alignment: Alignment.centerLeft,
-                                    child: SelectableText(
-                                      'bleh',
-                                      textAlign: TextAlign.left,
-                                      style: TextStyle(color: theme.primaryColor, fontSize: 15),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
                           ],
                         ),
                       ),
