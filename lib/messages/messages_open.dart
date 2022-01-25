bc8cd260806a80625d1ca1bd7d0d003b4984f194
FlutLab.io <>
2022-01-25 09:17:05 +0000

    Autosave

--- lib/messages/messages_open.dart
+++ lib/messages/messages_open.dart
@@ -171,7 +171,7 @@ class messages_openState extends State<messages_open> {
           child: Row(
             children: <Widget>[
               Container(
-                width: MediaQuery.of(context).size.width / 1,
+                width: MediaQuery.of(context).size.width / 2,
                 child: TextField(
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
