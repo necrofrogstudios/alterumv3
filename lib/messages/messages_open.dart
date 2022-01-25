19d97d83441d50d364d6b49d6b5a1169a229cc1c
FlutLab.io <>
2022-01-25 10:14:59 +0000

    Autosave

--- lib/messages/messages_open.dart
+++ lib/messages/messages_open.dart
@@ -166,7 +166,7 @@ class partner_messages extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Container(
-                        width: MediaQuery.of(context).size.width / 1.3,
+                        width: MediaQuery.of(context).size.width / 1.2,
                         child: SelectableText(
                           'blep blep blep blep blep blep blep blep blep blep blep blep blep blep /n'
                           'test blep',
