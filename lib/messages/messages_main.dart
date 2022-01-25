d6947f4151840f33a1a660a6a9f0d34564ea1fc6
FlutLab.io <>
2022-01-25 08:33:59 +0000

    Autosave

--- lib/messages/messages_main.dart
+++ lib/messages/messages_main.dart
@@ -69,7 +69,7 @@ class messages_mainState extends State<messages_main> {
             Padding(
               padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
               child: Column(
-                mainAxisSize: MainAxisSize.max,
+                mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   Padding(
                     padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
