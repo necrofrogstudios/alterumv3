9e2e1a429bbc443be248e6c315a62954e521838d
FlutLab.io <>
2022-01-25 09:37:01 +0000

    Autosave

--- lib/messages/messages_open.dart
+++ lib/messages/messages_open.dart
@@ -66,94 +66,91 @@ class messages_openState extends State<messages_open> {
             Container(color: theme.splashColor, height: 4),
             profile_top_buttons(),
             Container(color: theme.splashColor, height: 4),
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                    child: Text(
-                      'Guarded',
-                      style: TextStyle(
-                        fontSize: 30,
-                        color: theme.primaryColor,
-                      ),
+            Column(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: Text(
+                    'Guarded',
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: theme.primaryColor,
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
-                    child: Container(
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          side: BorderSide(
-                            color: theme.splashColor,
-                            width: 4.0,
-                          ),
-                          borderRadius: BorderRadius.circular(
-                            20.0,
-                          ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                  child: Container(
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                          color: theme.splashColor,
+                          width: 4.0,
                         ),
-                        color: theme.accentColor,
-                        child: Column(
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.fromLTRB(15, 15, 0, 15),
-                                      child: Container(
-                                        height: 50,
-                                        width: 50,
-                                        child: ClipRRect(
-                                          borderRadius: BorderRadius.circular(10.0),
-                                          child: Image.network('https://simg.nicepng.com/png/small/8-87422_alien-comments-alien-avatar-red.png'),
-                                        ),
+                        borderRadius: BorderRadius.circular(
+                          20.0,
+                        ),
+                      ),
+                      color: theme.accentColor,
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.fromLTRB(15, 15, 0, 15),
+                                    child: Container(
+                                      height: 50,
+                                      width: 50,
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(10.0),
+                                        child: Image.network('https://simg.nicepng.com/png/small/8-87422_alien-comments-alien-avatar-red.png'),
                                       ),
                                     ),
-                                    Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        Padding(
-                                          padding: EdgeInsets.fromLTRB(10, 7, 0, 0),
-                                          child: Text(
-                                            'Guarded',
-                                            style: TextStyle(color: theme.primaryColor, fontSize: 15),
-                                          ),
-                                        ),
-                                        Padding(
-                                          padding: EdgeInsets.fromLTRB(10, 5, 0, 10),
-                                          child: Text(
-                                            '01/25/2022 - 12:40 AM',
-                                            style: TextStyle(fontSize: 15, color: theme.primaryColor.withOpacity(0.45)),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.fromLTRB(0, 0, 5, 0),
-                                  child: IconButton(
-                                    splashColor: Colors.transparent,
-                                    highlightColor: Colors.transparent,
-                                    hoverColor: Colors.transparent,
-                                    iconSize: 15,
-                                    icon: Icon(Icons.close, color: theme.primaryColor),
                                   ),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Padding(
+                                        padding: EdgeInsets.fromLTRB(10, 7, 0, 0),
+                                        child: Text(
+                                          'Guarded',
+                                          style: TextStyle(color: theme.primaryColor, fontSize: 15),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.fromLTRB(10, 5, 0, 10),
+                                        child: Text(
+                                          '01/25/2022 - 12:40 AM',
+                                          style: TextStyle(fontSize: 15, color: theme.primaryColor.withOpacity(0.45)),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              Padding(
+                                padding: EdgeInsets.fromLTRB(0, 0, 5, 0),
+                                child: IconButton(
+                                  splashColor: Colors.transparent,
+                                  highlightColor: Colors.transparent,
+                                  hoverColor: Colors.transparent,
+                                  iconSize: 15,
+                                  icon: Icon(Icons.close, color: theme.primaryColor),
                                 ),
-                              ],
-                            ),
-                          ],
-                        ),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ],
         ),
@@ -197,6 +194,65 @@ class messages_openState extends State<messages_open> {
           ),
         ),
       ),
+    );
+  }
+}
+
+class partner_messages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    return Stack(
+      children: [
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 10, 50, 5),
+          child: Container(
+            height: 48,
+            decoration: BoxDecoration(
+              color: theme.splashColor,
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(40.0),
+                topRight: Radius.circular(40.0),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 14, 54, 0),
+          child: Container(
+            height: 40,
+            decoration: BoxDecoration(
+              color: theme.accentColor,
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(40.0),
+                topRight: Radius.circular(40.0),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(10, 14, 60, 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                items[i],
+                style: TextStyle(
+                  fontSize: 20,
+                  color: theme.primaryColor,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(
+                  Icons.arrow_forward,
+                  color: theme.primaryColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
