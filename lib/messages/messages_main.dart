cf200e0df61d5dba0d1fdf8cdb659c7b08748026
FlutLab.io <>
2022-01-25 06:26:16 +0000

    Autosave

--- lib/messages/messages_main.dart
+++ lib/messages/messages_main.dart
@@ -73,10 +73,7 @@ class messages_mainState extends State<messages_main> {
                   Container(
                     decoration: const BoxDecoration(
                       border: Border(
-                        top: BorderSide(width: 1.0, color: Color(0xFFDFDFDF)),
-                        left: BorderSide(width: 1.0, color: Color(0xFFDFDFDF)),
-                        right: BorderSide(width: 1.0, color: Color(0xFF7F7F7F)),
-                        bottom: BorderSide(width: 1.0, color: Color(0xFF7F7F7F)),
+                        side: BorderSide(width: 1.0, color: Color(0xFFDFDFDF)),
                       ),
                       color: Color(0xFFBFBFBF),
                     ),
