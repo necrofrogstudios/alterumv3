b39208ea838a15f4704b7a8a8cddc6920c1f39d1
FlutLab.io <>
2022-01-25 03:32:16 +0000

    Autosave

--- lib/popular_widgets/drawer.dart
+++ lib/popular_widgets/drawer.dart
@@ -245,13 +245,17 @@ class drawer extends StatelessWidget {
 class OvalRightBorderClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
-    var path = Path();
-    path.lineTo(0, 0);
-    path.lineTo(size.width - 40, 0);
-    path.quadraticBezierTo(size.width, size.height / 4, size.width, size.height / 2);
-    path.quadraticBezierTo(size.width, size.height - (size.height / 4), size.width - 40, size.height);
-    path.lineTo(0, size.height);
+    final path = Path();
+    path.lineTo(size.width, 0.0);
+    path.lineTo(size.width * 0.75, size.height);
+    path.lineTo(size.width * 0.25, size.height);
+    path.close();
     return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) {
+    return false;
   }
 }
 
