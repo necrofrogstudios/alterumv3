910eb04230f409ab3c59effb9039de8544599220
FlutLab.io <>
2022-01-25 06:47:20 +0000

    Autosave

--- lib/messages/messages_main.dart
+++ lib/messages/messages_main.dart
@@ -94,9 +94,9 @@ class messages_mainState extends State<messages_main> {
                                   children: [
                                     Padding(
                                       padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
-                                      child: CircleAvatar(
-                                        radius: 5,
-                                        backgroundImage: NetworkImage('https://simg.nicepng.com/png/small/8-87422_alien-comments-alien-avatar-red.png'),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(26.0),
+                                        child: Image.network('https://simg.nicepng.com/png/small/8-87422_alien-comments-alien-avatar-red.png'),
                                       ),
                                     ),
                                     Column(
