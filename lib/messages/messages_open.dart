4a68e1c34113538e7474425d14d0eef12e7bf345
FlutLab.io <>
2022-01-26 02:45:57 +0000

    Autosave

--- lib/messages/messages_open.dart
+++ lib/messages/messages_open.dart
@@ -175,10 +175,10 @@ class partner_messages extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
                           Padding(
-                            padding: EdgeInsets.fromLTRB(10, 0, 5, 0),
+                            padding: EdgeInsets.fromLTRB(10, 0, 5, 3),
                             child: Container(
                               child: SelectableText(
-                                'Guarded',
+                                'basketcase101',
                                 style: TextStyle(
                                   fontSize: 15,
                                   color: theme.primaryColor,
