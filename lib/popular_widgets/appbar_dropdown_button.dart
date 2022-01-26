dc3dffde0e6c3b3583124e7ad61132f80aa7bebb
FlutLab.io <>
2022-01-26 08:59:18 +0000

    Autosave

--- lib/popular_widgets/appbar_dropdown_button.dart
+++ lib/popular_widgets/appbar_dropdown_button.dart
@@ -86,7 +86,7 @@ class _SimpleAccountMenuState extends State<SimpleAccountMenu> with SingleTicker
             padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
             child: pressed == true
                 ? Badge(
-                    position: BadgePosition.topEnd(top: 10, end: -10),
+                    position: BadgePosition.topEnd(top: 10, end: -8),
                     badgeContent: null,
                     child: IconButton(
                       icon: Icon(Icons.notifications),
