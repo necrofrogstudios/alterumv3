43a90b75d5356dfeafe6678e60a40120cf1e8e39
FlutLab.io <>
2022-01-26 02:57:23 +0000

    Autosave

--- lib/messages/messages_open.dart
+++ lib/messages/messages_open.dart
@@ -182,6 +182,7 @@ class partner_messages extends StatelessWidget {
                                 style: TextStyle(
                                   fontSize: 15,
                                   color: theme.primaryColor,
+                                  decoration: TextDecoration.underline,
                                 ),
                               ),
                             ),
